c0099c35c70765df7889f6e08c28a702c74ab111
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-07-10 07:06:59 +0000

    script to automatically make redirection pages for capitalized days
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@15 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ autoredir.py
@@ -0,0 +1,57 @@
+#
+# Script to check language links for years A.D.
+#
+# (C) Rob W.W. Hooft, 2003
+# Distribute under the terms of the GPL.
+
+import os,sys,re,wikipedia
+
+# Summary used in the modification request
+wikipedia.setAction('redirect')
+
+debug=0
+if debug:
+    wikipedia.langs={'test':'test.wikipedia.org'}
+    mylang='test'
+else:
+    mylang='nl'
+
+def do(month):
+    if debug:
+        page='Robottest'
+
+    for day in range(1,31+1):
+        print day
+        page="%d %s"%(day,month)
+        cappage="%d_%s"%(day,month.capitalize())
+
+        do=1
+        
+        # Only if the page exists
+        try:        
+            text=wikipedia.getPage(mylang,page)
+        except wikipedia.NoPage:
+            print "Page does not exist"
+            do=0
+        
+        # Only if the redir-page does not exist
+        try:        
+            text=wikipedia.getPage(mylang,cappage)
+        except wikipedia.NoPage:
+            pass
+        else:
+            print "Redirect exists"
+            do=0
+
+        if do==0:
+            continue
+
+        text="#redirect [[%s]]"%page
+
+        status,reason,data=wikipedia.putPage(mylang,cappage,text)
+        print status,reason
+
+if __name__=="__main__":
+    do('september')
+    do('december')
+
