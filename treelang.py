6b2798f63f8c55fd976e6021daefc116fcb36af1
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-07-12 09:03:05 +0000

    make it work
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@19 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- treelang.py
+++ treelang.py
@@ -13,7 +13,8 @@ mylang = 'nl'
 # Summary used in the modification request
 wikipedia.setAction('Rob Hooft: semi-automatic interwiki script')
 
-debug = 0
+debug = 1
+forreal = 1
 
 def compareLanguages(old,new):
     removing=[]
@@ -30,7 +31,7 @@ def compareLanguages(old,new):
     if removing:
         s=s+" Removing:"+",".join(removing)
     return s
-
+    
 def treestep(arr,code,name):
     assert arr[code,name] is None
     try:
@@ -46,12 +47,13 @@ def treestep(arr,code,name):
         return 0
     arr[code,name]=text
     for newcode,newname in wikipedia.getLanguageLinks(text).iteritems():
+        # Recognize and standardize for Wikipedia
         newname=newname[0].upper()+newname[1:]
+        newname=newname.strip()
+        newname=wikipedia.link2url(newname)
         if not (newcode,newname) in arr:
-            try:
-                print "NOTE: from %s:%s we got the new %s:%s"%(code,name,newcode,newname)
-            except ValueError: 
-                print "NOTE: new:", (code,name,newcode,newname)
+            lname=wikipedia.url2link(newname)
+            print "NOTE: from %s:%s we got the new %s:%s"%(code,name,newcode,lname)
             arr[newcode,newname]=None
             n+=1
     return n
@@ -64,7 +66,48 @@ def treesearch(code,name):
         for newcode,newname in arr.keys():
             if arr[newcode,newname] is None:
                 modifications+=treestep(arr,newcode,newname)
+    return arr
     
 name=raw_input('Which page to check:')
 
+name=wikipedia.link2url(name)
+
 m=treesearch(mylang,name)
+print "==Result=="
+new={}
+k=m.keys()
+k.sort()
+for code,cname in k:
+    if code==mylang:
+        if m[code,cname]:
+            old=wikipedia.getLanguageLinks(m[code,cname])
+            oldtext=m[code,cname]
+    elif m[(code,cname)]:
+        print "%s:%s"%(code,wikipedia.url2link(cname))
+        if new.has_key(code):
+            print "ERROR: %s has '%s' as well as '%s'"%(code,new[code],wikipedia.url2link(cname))
+            while 1:
+                answer=raw_input("Use former (f) or latter (l)?")
+                if answer.startswith('f'):
+                    break
+                elif answer.startswith('l'):
+                    new[code]=wikipedia.url2link(cname)
+                    break
+        else:
+            new[code]=wikipedia.url2link(cname)
+print "==status=="
+#print old
+print compareLanguages(old,new)
+print "==upload=="
+s=wikipedia.interwikiFormat(new)
+newtext=s+wikipedia.removeLanguageLinks(oldtext)
+if debug:
+    print s
+if newtext!=oldtext:
+    print "NOTE: Replacing %s: %s"%(mylang,name)
+    if forreal:
+        answer=raw_input('submit y/n ?')
+        if answer=='y':
+            status,reason,data=wikipedia.putPage(mylang,name,newtext)
+            if str(status)!='302':
+                print status,reason
