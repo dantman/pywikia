81f89450f4dd21b4437405e17022cb9d72a49234
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-07-15 12:58:00 +0000

    script to extract page-links from a html dump of a wikipedia page
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@28 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ extract_names.py
@@ -0,0 +1,10 @@
+import sys,re
+R=re.compile('/wiki/(.*?)" *class=\'internal')
+fn=sys.argv[1]
+f=open(fn)
+text=f.read()
+f.close()
+for hit in R.findall(text):
+    if not ':' in hit:
+        if not hit in ['Hoofdpagina','In_het_niewa']:
+            print hit
