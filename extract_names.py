30816ca2210bb681a02a0a855dd55ee55f8530d1
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-07-17 20:53:18 +0000

    Allow doublequote, typo
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@31 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- extract_names.py
+++ extract_names.py
@@ -1,10 +1,10 @@
 import sys,re
-R=re.compile('/wiki/(.*?)" *class=\'internal')
+R=re.compile('/wiki/(.*?)" *class=[\'\"]internal')
 fn=sys.argv[1]
 f=open(fn)
 text=f.read()
 f.close()
 for hit in R.findall(text):
     if not ':' in hit:
-        if not hit in ['Hoofdpagina','In_het_niewa']:
+        if not hit in ['Hoofdpagina','In_het_niews']:
             print hit
