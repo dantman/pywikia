6534bfc54e004c8084df405f54783a2f7fd4421a
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-09-30 11:11:06 +0000

    * header  * use wikipedia.mylang  * use treelang.py -backlink argument
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@109 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- treelang_all.py
+++ treelang_all.py
@@ -1,3 +1,10 @@
+# Loop over all pages, calling treelang for each
+#
+# $Id$
+#
+# (C) Rob W.W. Hooft, 2003
+# Distribute under the terms of the GPL.
+
 import os,wikipedia,sys
 
 if sys.platform=='win32':
@@ -5,14 +12,14 @@ if sys.platform=='win32':
 else:
     normalstatus=0,256
     
-for f in wikipedia.allnlpages(start=sys.argv[1]):
+for f in wikipedia.allpages(start=sys.argv[1]):
     f=f.replace("'",r"'\''")
     print
     print repr(f)
     if sys.platform=='win32':
-        status=os.system("python2.3 treelang.py -autonomous %s"%f)
+        status=os.system("python treelang.py -backlink -autonomous %s"%f)
     else:
-        status=os.system("python2.3 treelang.py -autonomous '%s'"%f)
+        status=os.system("python treelang.py -backlink -autonomous '%s'"%f)
     if status not in normalstatus:
         print "Exit status ",status
         sys.exit(1)
