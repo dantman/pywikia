e49600d56314e7b279581a1dac1d8791dd4beb4b
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-10-18 06:40:26 +0000

    login script to use username/password for wikipedia
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@115 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ login.py
@@ -0,0 +1,49 @@
+import re
+import httplib
+import wikipedia
+
+# This needs to be translated for each wikipedia we want to use it on.
+loginaddr={'nl':'/w/wiki.phtml?title=Speciaal:Userlogin&amp;action=submit',
+           }
+
+if 0:
+    print wikipedia.getPage(wikipedia.mylang, 'Speciaal:Userlogin',do_edit=0)
+    import sys
+    sys.exit(0)
+
+print "Logging in to ",wikipedia.langs[wikipedia.mylang]
+username=raw_input('username: ')
+password=raw_input('password: ')
+
+# I hope this doesn't need translation
+data = wikipedia.urlencode((
+            ('wpName', username),
+            ('wpPassword', password),
+            ('wpLoginattempt', "Aanmelden & Inschrijven"),
+            ('wpRemember', '1'),
+            ))
+
+headers = {"Content-type": "application/x-www-form-urlencoded"}
+conn = httplib.HTTPConnection(wikipedia.langs[wikipedia.mylang])
+conn.request("POST", loginaddr[wikipedia.mylang], data, headers)
+response = conn.getresponse()
+data = response.read()
+conn.close()
+#print response.status, response.reason
+#print data
+#print dir(response)
+f=open('login.data','w')
+n=0
+msg=response.msg
+Reat=re.compile(': (.*?);')
+print repr(msg.getallmatchingheaders('set-cookie'))
+for eat in msg.getallmatchingheaders('set-cookie'):
+    m=Reat.search(eat)
+    if m:
+        n=n+1
+        f.write(m.group(1)+'\n')
+f.close()
+if n==4:
+    print "Should be logged in now"
+else:
+    print "Hm. Did something go wrong?"
