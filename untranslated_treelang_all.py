8d6ef158800050b1f41c11af31efe26d7392919e
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-10-25 15:45:07 +0000

    Like treelang_all, but now for extended (untranslated) rather than automatic (autonomous) treelang-checking
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@143 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ untranslated_treelang_all.py
@@ -0,0 +1,33 @@
+"""
+Loop over all pages in the home wikipedia, calling untranslated treelang for each
+"""
+#
+# (C) Rob W.W. Hooft, 2003
+#
+# Distribute under the terms of the PSF license.
+#
+__version__='$Id$'
+#
+import os,wikipedia,sys
+
+if sys.platform=='win32':
+    normalstatus=0,1
+else:
+    normalstatus=0,256
+    
+for pl in wikipedia.allpages(start=sys.argv[1]):
+    f=pl.urlname()
+    wikipedia.throttle()
+    f=f.replace("'",r"'\''")
+    if os.isatty(1):
+        print
+        print repr(f)
+    if sys.platform=='win32':
+        status=os.system("..\python treelang.py -untranslated -confirm %s"%f)
+    else:
+        status=os.system("python treelang.py -untranslated -confirm '%s'"%f)
+    if status not in normalstatus:
+        print "Exit status ",status
+        sys.exit(1)
+
+
