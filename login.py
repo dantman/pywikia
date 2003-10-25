19ac460f8e2a763210b59fd18a08f1a61e33ee9b
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-10-25 07:38:45 +0000

    explain at top; use translations of special instead of explicit list of login addresses; check login name for en: wikipedia
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@126 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- login.py
+++ login.py
@@ -3,6 +3,15 @@ Script to log the robot in to a wikipedia account.
 
 Suggestion is to make a special account to use for robot use only. Make
 sure this robot account is well known on your home wikipedia before using.
+
+This script has no command line arguments. Please run it as such. It will ask
+for your username and password (it will show your password on the screen!), log
+in to your home wikipedia using this combination, and store the resulting
+cookies (containing your password in encoded form, so keep it secured!) in a
+file named login.data
+
+The wikipedia library will be looking for this file and will use the login
+information if it is present.
 """
 #
 # (C) Rob W.W. Hooft, 2003
@@ -15,19 +24,27 @@ import re
 import httplib
 import wikipedia
 
-# This needs to be translated for each wikipedia we want to use it on.
-loginaddr={'nl':'/w/wiki.phtml?title=Speciaal:Userlogin&amp;action=submit',
-           }
-
-if 0:
-    print wikipedia.getPage(wikipedia.mylang, 'Speciaal:Userlogin',do_edit=0)
+if not wikipedia.special.has_key(wikipedia.mylang):
+    print "Please add the translation for the Special: namespace in"
+    print "Your home wikipedia to the wikipedia.py module"
     import sys
-    sys.exit(0)
+    sys.exit(1)
+    
+# This needs to be translated for each wikipedia we want to use it on.
+loginaddr='/w/wiki.phtml?title=%s:Userlogin&amp;action=submit'
 
 print "Logging in to ",wikipedia.langs[wikipedia.mylang]
 username=raw_input('username: ')
 password=raw_input('password: ')
 
+if wikipedia.mylang=='en':
+    pl=wikipedia.PageLink('en','Wikipedia:Bots')
+    text=pl.get()
+    if not '[[User:%s'%username in text:
+        print "Your username is not listed on [[Wikipedia:Bots]]"
+        print "Please make sure you are allowed to use the robot"
+        print "Before actually using it!"
+        
 # I hope this doesn't need translation
 data = wikipedia.urlencode((
             ('wpName', username),
@@ -38,7 +55,8 @@ data = wikipedia.urlencode((
 
 headers = {"Content-type": "application/x-www-form-urlencoded"}
 conn = httplib.HTTPConnection(wikipedia.langs[wikipedia.mylang])
-conn.request("POST", loginaddr[wikipedia.mylang], data, headers)
+pagename = loginaddr%wikipedia.special[wikipedia.mylang]
+conn.request("POST", pagename, data, headers)
 response = conn.getresponse()
 data = response.read()
 conn.close()
@@ -56,7 +74,8 @@ for eat in msg.getallmatchingheaders('set-cookie'):
         n=n+1
         f.write(m.group(1)+'\n')
 f.close()
+
 if n==4:
     print "Should be logged in now"
 else:
-    print "Hm. Did something go wrong?"
+    print "Hm. Did something go wrong? Wrong password?"
