37d98eb126d27dfc656b3b76a0e77199ddeb77b6
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-12-04 16:40:04 +0000

    This runs solve_disambiguation over a list of pages, like treelang_all does with treelang. First version, stripped of all extras.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@259 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ disamb_all.py
@@ -0,0 +1,51 @@
+"""
+This runs solve_disambiguation.py over a list of pages, which
+can for example be gotten through extract_names.py. Its only
+argument is the name of the file from which the list is taken.
+"""
+#
+# (C) Rob W.W. Hooft, Andre Engels, 2003
+#
+# Distribute under the terms of the PSF license.
+#
+#
+import os,sys
+
+import wikipedia
+
+file=[]
+
+for arg in sys.argv[1:]:
+    file.append(arg)
+
+if sys.platform == 'win32':
+    normalstatus = 0, 1
+else:
+    normalstatus = 0, 256
+
+lst=[]
+
+for fn in file:
+    f=open(fn)
+    for line in f.readlines():
+        lst.append(wikipedia.PageLink(wikipedia.mylang,line))
+    f.close()
+        
+for pl in lst:
+    f = pl.urlname()
+    f = f.replace("'", r"'\''")
+    if os.isatty(1):
+        print
+        print repr(f)
+    hintstr=''
+    if sys.platform=='win32':
+        status = os.system("solve_disambiguation.py %s" % (f))
+    else:
+        if debug:
+            print ("python treelang.py '%s'" % (f))
+            status = 0
+        else:
+            status = os.system("python treelang.py '%s'" % (f))
+    if status not in normalstatus:
+        print "Exit status ", status
+        sys.exit(1)
