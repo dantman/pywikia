d7da9f5817a394f3d89b6dd31abaa5b6b80938bd
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-12-13 22:15:00 +0000

    modify brackethttp to use PageLinks
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@308 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- brackethttp.py
+++ brackethttp.py
@@ -3,21 +3,33 @@ Script to correct URLs like
 (http://www.example.org) to [http://www.example.org example.org]
 to have correct generation of links in Wikipedia
 """
-
+#
 # (C) 2003 Thomas R. Koll, <tomk32@tomk32.de>
+#
+# Distribute under the terms of the PSF license.
+#
 __version__='$Id$'
+#
+import re, sys
 
-import re,sys,wikipedia
+import wikipedia
 
-mylang = "de"
 myComment = 'Bot: URL fixed'
 
 for arg in sys.argv[1:]:
-    text= wikipedia.getPage(mylang, arg)
-
-    newText = re.sub("\((http:\/\/([^ ]*))\)", "([\\1 \\2])", text)
-    if newText!=text:
-       status,reason,data = wikipedia.putPage(mylang, arg ,newText, myComment)
-       print status,reason
+    if wikipedia.argHandler(arg):
+        pass
     else:
-       print "No bad link found"
+        pl = wikipedia.PageLink(wikipedia.mylang, arg)
+        text = pl.get()
+        
+        newText = re.sub("\((http:\/\/([^ ]*))\)", "([\\1 \\2])", text)
+        if newText != text:
+            import difflib
+            for line in difflib.ndiff(text.split('\r\n'),newText.split('\r\n')):
+                if line[0] in ['+','-']:
+                    print repr(line)[2:-1]
+            status, reason, data = pl.put(newText, myComment)
+            print status, reason
+        else:
+            print "No bad link found"
