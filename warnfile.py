4f198df7c885f53843c69bef34b2c8b2184e6205
christianlist <christianlist@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2003-12-23 16:41:55 +0000

    Localize messages
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@400 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- warnfile.py
+++ warnfile.py
@@ -81,6 +81,8 @@ def ReadWarnfile(fn):
             
 
 if __name__ == "__main__":
+    if interwiki.msg.has_key(wikipedia.mylang):
+        interwiki.globalvar.msglang = wikipedia.mylang
     for arg in sys.argv[1:]:
         if wikipedia.argHandler(arg):
             pass
