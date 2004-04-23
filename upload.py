4c2add8d9b9f30a9e28adbaf14ef9cc7313d74f7
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-04-23 07:59:30 +0000

    When given arguments, those are the filenames/URLs to upload.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@607 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- upload.py
+++ upload.py
@@ -4,12 +4,15 @@ Script to upload images to wikipedia.
 Arguments:
 
   -lang:xx Log in to the given wikipedia language
+
+Any other arguments given are filenames or URLs to get the pictures
+from.
   
 The script will ask for the location of an image, and for a description.
 It will then send the image to wikipedia.
 """
 #
-# (C) Rob W.W. Hooft, 2003
+# (C) Rob W.W. Hooft, Andre Engels 2003-2004
 #
 # Distribute under the terms of the PSF license.
 #
@@ -18,27 +21,24 @@ __version__='$Id$'
 import re,sys
 import wikipedia, lib_images
 
+flist = []
 
 for arg in sys.argv[1:]:
     if wikipedia.argHandler(arg):
         pass
     else:
-        print "Unknown argument: ",arg
-        sys.exit(1)
-        
+        flist.append(arg)
+
 if not wikipedia.cookies:
     print "You must be logged in to upload images"
     import sys
     sys.exit(1)
-    
-uploadaddr='/wiki/%s:Upload'%wikipedia.special[wikipedia.mylang]
 
-print "Uploading image to ",wikipedia.langs[wikipedia.mylang]
-
-def main():
+if flist==[]:
     fn = raw_input('File or URL where image is now : ')
     lib_images.get_image(fn, wikipedia.mylang,"")
-
-if __name__=="__main__":
-    main()
-    
+else:
+    for fn in flist:
+        print('')
+        print('Uploading: %s')%fn
+        lib_images.get_image(fn, wikipedia.mylang,"")
