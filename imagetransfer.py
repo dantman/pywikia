6c35e8f39e1ffe8f8b03d75632a949b14164502d
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-05-09 12:14:32 +0000

    introduced arguments -from and -file
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@664 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- imagetransfer.py
+++ imagetransfer.py
@@ -8,8 +8,13 @@ Arguments:
 
   -lang:xx Log in to the given wikipedia language
   
-The script will ask for the location of an image, and for a description.
-It will then send the image to wikipedia.
+  -from:xx Only follow interwiki links to language xx. Argument can be used
+           multiple times to indicate that only some specific languages should
+           be followed. If this argument isn't used, all interwiki links will
+           be followed.
+  
+The script will ask for a page title and eventually copy images from the
+equivalent pages on other wikipedias.
 """
 #
 # (C) Andre Engels, 2004
@@ -22,65 +27,96 @@ import re,sys,string
 import httplib
 import wikipedia, lib_images
 
-inname = []
+# if the -file argument is used, page titles are dumped in this array.
+# otherwise it will only contain one page.
+page_list = []
+# if -file is not used, this temporary array is used to read the page title.
+page_title = []
 imagelist = []
+source_wikis = []
 
 for arg in sys.argv[1:]:
     if wikipedia.argHandler(arg):
         pass
+    elif arg.startswith('-from:'):
+        source_wikis.append(arg[6:])
+    elif arg.startswith('-file'):
+        if len(arg) == 5:
+            # todo: check for console encoding to allow special characters
+            # in filenames, as done below with pagename
+            file = raw_input('Please enter the list\'s filename: ')
+        else:
+            file = arg[6:]
+        # open file and read page titles out of it
+        f=open(file)
+        for line in f.readlines():
+            if line != '\n':           
+                page_list.append(line)
+        f.close()
     else:
-        inname.append(arg)
+        page_title.append(arg)
 
-inname = '_'.join(inname)
+# if the disambiguation page is given as a command line argument,
+# connect the title's parts with spaces
+if page_title != []:
+     page_title = ' '.join(page_title)
+     page_list.append(page_title)
 
+# if no page title was given as an argument, and none was
+# read from a file, query the user
+if page_list == []:
+    pagename = raw_input('Which page to check: ')
+    pagename = unicode(pagename, config.console_encoding)
+    pagename = pagename.encode(wikipedia.code2encoding(wikipedia.mylang))
+    page_list.append(pagename)
+    
 if not wikipedia.cookies:
     print "You must be logged in to upload images"
     import sys
     sys.exit(1)
     
-if inname=='':
-    inname=raw_input("For which page are images to be found: ")
-
-inpl = wikipedia.PageLink(wikipedia.mylang, inname)
-ilinks = inpl.interwiki()
-
-for page in ilinks:
-    lang=page.code()
-    try:
-        for i in page.imagelinks():
-            imagelist.append(i)
-    except wikipedia.NoPage:
-        pass
-    except wikipedia.IsRedirectPage,arg:
-        page2=wikipedia.PageLink(page.code(),arg.args[0])
+for page_list_item in page_list:
+    inpl = wikipedia.PageLink(wikipedia.mylang, page_list_item)
+    ilinks = inpl.interwiki()
+    
+    for page in ilinks:
+        if page.code() in source_wikis or source_wikis==[]:
+            lang=page.code()
+            try:
+                for i in page.imagelinks():
+                    imagelist.append(i)
+            except wikipedia.NoPage:
+                pass
+            except wikipedia.IsRedirectPage,arg:
+                page2=wikipedia.PageLink(page.code(),arg.args[0])
+                try:
+                    for i in page2.imagelinks():
+                        imagelist.append(i)
+                except wikipedia.NoPage:
+                    pass
+                except wikipedia.IsRedirectPage:
+                    pass
+    
+    for i in range(len(imagelist)):
+        imagelink = imagelist[i]
+        print "--------------------------------------------------"
+        print "%s. Found image: %s"% (i,imagelink.aslink())
         try:
-            for i in page2.imagelinks():
-                imagelist.append(i)
+            print wikipedia.UnicodeToAsciiHtml(imagelink.get())
         except wikipedia.NoPage:
-            pass
+            print "Description empty."
         except wikipedia.IsRedirectPage:
-            pass
-
-for i in range(len(imagelist)):
-    imagelink = imagelist[i]
-    print "--------------------------------------------------"
-    print "%s. Found image: %s"% (i,imagelink.aslink())
-    try:
-        print wikipedia.UnicodeToAsciiHtml(imagelink.get())
-    except wikipedia.NoPage:
-        print "Description empty."
-    except wikipedia.IsRedirectPage:
-        print "Description page is redirect?!"
-
-print "=================================================="
-
-while len(imagelist)>0:
-    print("Give the number of the image to upload.")
-    todo=raw_input("To end uploading, press enter: ")
-    if not todo:
-        break
-    todo=int(todo)
-    if todo in range(len(imagelist)):
-        lib_images.transfer_image(imagelist[todo])
-    else:
-        print("No such image number.")
+            print "Description page is redirect?!"
+    
+    print "=================================================="
+    
+    while len(imagelist)>0:
+        print("Give the number of the image to upload.")
+        todo=raw_input("To end uploading, press enter: ")
+        if not todo:
+            break
+        todo=int(todo)
+        if todo in range(len(imagelist)):
+            lib_images.transfer_image(imagelist[todo])
+        else:
+            print("No such image number.")
