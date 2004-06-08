54c6246a952de088112e54dc9eeeddf5dd74b685
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-06-08 21:43:07 +0000

    rewritten
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@702 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- catlib.py
+++ catlib.py
@@ -11,11 +11,11 @@ __version__ = '$Id$'
 import wikipedia
 
 class _CatLink(wikipedia.PageLink):
+    #returns a list of the titles of all subcategories and all articles in this category
     def catlist(self, recurse = 0):
         import re
         Rtitle = re.compile('title=\"([^\"]*)\"')
         ns = wikipedia.family.category_namespaces(wikipedia.mylang)
-        state = 0
         catsdone = []
         catstodo = [self]
         pages = []
@@ -23,38 +23,62 @@ class _CatLink(wikipedia.PageLink):
             cat = catstodo.pop()
             catsdone.append(cat)
             txt = wikipedia.getPage(cat.code(), cat.urlname(), do_edit = 0)
-            ibegin = txt.index('<div id="content">')
-            iend = txt.index('<div id="catlinks">')
+            # index where subcategory listing begins
+            # this only works for the current version of the MonoBook skin
+            ibegin = txt.index('<br style="clear:both;" />')
+            # index where article listing ends
+            iend = txt.index('<!-- end content -->')
             txt = txt[ibegin:iend]
             for title in Rtitle.findall(txt):
-                if state == 0:
-                    for ins in ns:
-                        if title.startswith(ins):
-                            state = 1
-                            break
-                if state == 1:
-                    for ins in ns:
-                        if title.startswith(ins):
-                            break
-                    else:
-                        state = 2
-                if state == 1:
-                    ncat = _CatLink(self.code(), title)
-                    if recurse and ncat not in catsdone:
-                        catstodo.append(ncat)
-                elif state == 2:
-                    npage = wikipedia.PageLink(self.code(), title)
-                    pages.append(npage)
+                # try different possibilities for namespaces (first letter lowercase, English 'category')
+                for ins in ns:
+                    # if the current link points at a subcategory page
+                    if title.startswith(ins + ':'):
+                        ncat = _CatLink(self.code(), title)
+                        if recurse and ncat not in catsdone:
+                            catstodo.append(ncat)
+                pages.append(title)
+        # TODO: filter duplicate results
         return pages
     
+    #returns a list of all subcategories in this category
+    def subcategories(self, recurse = 0):
+        ns = wikipedia.family.category_namespaces(wikipedia.mylang)
+        subcats = []
+        for title in self.catlist(recurse):
+            for ins in ns:
+                # if the current link points at a subcategory page
+                if title.startswith(ins + ':'):
+                    ncat = _CatLink(self.code(), title)
+                    subcats.append(ncat)
+        return subcats
+    
+    #returns a list of all articles in this category
+    def articles(self, recurse = 0):
+        ns = wikipedia.family.category_namespaces(wikipedia.mylang)
+        articles = []
+        titles = self.catlist(recurse)
+        for title in titles:
+            for ins in ns:
+                # if the current link points to a subcategory
+                if title.startswith(ins + ':'):
+                    # don't return this link
+                    titles.remove(title)
+        for title in titles:
+            npage = wikipedia.PageLink(self.code(), title)
+            articles.append(npage)
+        return articles
+
+     #TODO: create supercategories() function
+
 def CatLink(s):
     ns = wikipedia.family.category_namespaces(wikipedia.mylang)[0]
     return _CatLink(wikipedia.mylang, "%s:%s"%(ns, s))
 
 def test():
-    pl=CatLink('Biologie')
+    pl=CatLink('Software')
     
-    print pl.catlist(recurse = 1)
+    print pl.catlist(recurse = 0)
 
 if __name__=="__main__":
     import sys
