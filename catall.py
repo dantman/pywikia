13f0b72f84916f1b7068332a28687536acb13023
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-06-19 09:09:35 +0000

    A new bot for categories. It goes through a part of Special:Allpages, and asks for each page in which category/categories it should be placed.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@733 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ catall.py
@@ -0,0 +1,102 @@
+# -*- coding: cp1252 -*-
+"""
+Add or change categories on a number of pages. Usage:
+catall.py name - goes through pages, starting at 'name'. Provides
+ the categories on the page and asks whether to change them. If no
+ starting name is provided, the bot starts at 'A'.
+
+Options:
+-onlynew : Only run on pages that do not yet have a category.
+"""
+#
+# (C) Rob W.W. Hooft, Andre Engels, 2004
+#
+# Distribute under the terms of the PSF license.
+# 
+__version__ = '$Id$'
+#
+
+import wikipedia,sys
+
+# This is a purely interactive robot. We set the delays lower.
+wikipedia.get_throttle.setDelay(5)
+wikipedia.put_throttle.setDelay(10)
+
+msg={
+    'en': 'Changing categories',
+    'nl': 'Verandering van categorieen'
+    }
+
+def choosecats(pagetext):
+    chosen=[]
+    flag=False
+    length=1000
+    print ("Give the new categories, one per line.")
+    print ("Empty line: if the first, don't change. Otherwise: Ready.")
+    print ("-: I made a mistake, let me start over.")
+    print ("?: Give (more of) the text of the page.")
+    print ("xx: if the first, remove all categories and add no nwe.")
+    while flag == False:
+        choice=raw_input("? ")
+        if choice=="":
+            flag=True
+        elif choice=="-":
+            chosen=choosecats(pagetext)
+            flag=True
+        elif choice=="?":
+            print wikipedia.UnicodeToAsciiHtml(pagetext[0:length])
+            length = length+500
+        elif choice=="xx" and chosen==[]:
+            chosen = None
+            flag=True
+        else:
+            chosen.append(choice)
+    return chosen
+
+def make_categories(page,list):
+    pllist=[]
+    for p in list:
+        cattitle="%s:%s"%(wikipedia.family.category[wikipedia.mylang],p)
+        pllist.append(wikipedia.PageLink(wikipedia.mylang,cattitle))
+    page.put(wikipedia.replaceCategoryLinks(page.get(),pllist), comment=msg[wikipedia.chooselang(wikipedia.mylang,msg)])
+
+docorrections=True
+start=[]
+
+for arg in sys.argv[1:]:
+    if wikipedia.argHandler(arg):
+        pass
+    elif arg == '-onlynew':
+        docorrections=False
+    else:
+        start.append(arg)
+
+if start == []:
+    start='A'
+else:
+    start=' '.join(start)
+
+for p in wikipedia.allpages(start = start):
+    try:
+        text=p.get()
+        cats=p.categories()
+        if cats == []:
+            print "========== %s =========="%p.ascii_linkname()
+            print "No categories"
+            print "----------------------------------------"
+            newcats=choosecats(text)
+            if newcats <> [] and newcats <> None:
+                make_categories(p,newcats)
+        else:
+            if docorrections:
+                print "========== %s =========="%p.ascii_linkname()
+                for c in cats:
+                    print c.linkname()
+                print "----------------------------------------"
+                newcats=choosecats(text)
+                if newcats == None:
+                    make_categories(p,[])
+                elif newcats <> []:
+                    make_categories(p,newcats)
+    except wikipedia.IsRedirectPage:
+        pass
