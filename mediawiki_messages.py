d5cde2d34c7be669d9a47fd39b80dd84806bbc2e
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-07-06 17:19:25 +0000

    save files into subdirectory
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@792 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- mediawiki_messages.py
+++ mediawiki_messages.py
@@ -13,12 +13,12 @@ import re, sys, pickle, codecs
 
 def get(key):
     import os.path
-    if not os.path.exists('mediawiki-messages-%s.dat' % wikipedia.mylang):
+    if not os.path.exists('mediawiki_messages/mediawiki-messages-%s.dat' % wikipedia.mylang):
         refresh_messages()
     # TODO: It's quite inefficient to reload the file every time this function
     # is used. Maybe we can save its content the first time the function is
     # called.
-    f = open('mediawiki-messages-%s.dat' % wikipedia.mylang, 'r')
+    f = open('mediawiki_messages/mediawiki-messages-%s.dat' % wikipedia.mylang, 'r')
     dictionary = pickle.load(f)
     f.close()
     key = key[0].lower() + key[1:]
@@ -28,6 +28,26 @@ def get(key):
         # TODO: Throw exception instead?
         print 'ERROR: MediaWiki Key %s not found' % key 
 
+def makepath(path):
+    """ creates missing directories for the given path and
+        returns a normalized absolute version of the path.
+
+    - if the given path already exists in the filesystem
+      the filesystem is not modified.
+
+    - otherwise makepath creates directories along the given path
+      using the dirname() of the path. You may append
+      a '/' to the path if you want it to be a directory path.
+
+    from holger@trillke.net 2002/03/18
+    """
+    from os import makedirs
+    from os.path import normpath,dirname,exists,abspath
+
+    dpath = normpath(dirname(path))
+    if not exists(dpath): makedirs(dpath)
+    return normpath(abspath(path))
+    
 def refresh_messages():
     host = wikipedia.family.hostname(wikipedia.mylang)
     # broken redirect maintenance page's URL
@@ -38,6 +58,7 @@ def refresh_messages():
     #f=open('/home/daniel/allmessages.html', 'r')
     #allmessages =  f.read()
     
+    print 'Parsing MediaWiki messages...'
     # First group is MediaWiki key string. Second group is the current value string.
     itemR = re.compile("<tr bgcolor=\"#F0F0FF\">\n"
                      + "<td>\n"
@@ -58,8 +79,10 @@ def refresh_messages():
         # Key strings only contain ASCII characters, so we can use them as dictionary keys
         dictionary[item[0]] = unicode(item[1], wikipedia.code2encoding(wikipedia.mylang))
         # Save the dictionary to disk
-        # TODO: Put them into another directory
-        f = open('mediawiki-messages-%s.dat' % wikipedia.mylang, 'w')
+        # Create directory for these files, if it doesn't exist already
+        makepath('mediawiki_messages')
+        # The file is stored in the mediawiki_messages subdir. Create if necessary. 
+        f = open(makepath('mediawiki_messages/mediawiki-messages-%s.dat' % wikipedia.mylang), 'w')
         pickle.dump(dictionary, f)
         f.close()
     
