27f80ba3e51ea2550be44cbe2cb078a406031223
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-07-30 23:37:43 +0000

    fixed special namespace for sq:
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@929 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia_family.py
+++ wikipedia_family.py
@@ -166,7 +166,7 @@ special = {
     'ru': '%D0%A1%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5',
     'sk': '%C5%A0peci%C3%A1lne',
     'sl': 'Posebno',
-    'sq': 'Special',
+    'sq': 'Speciale',
     'sr': '%D0%9F%D0%BE%D1%81%D0%B5%D0%B1%D0%BD%D0%BE',
     'sv': 'Special',
     'uk': '%D0%A1%D0%BF%D0%B5%D1%86%D1%96%D0%B0%D0%BB%D1%8C%D0%BD%D1%96',
