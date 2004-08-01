cea7a88740ff0a5e8b2b8494b9edaf42c1949c04
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-01 12:51:09 +0000

    default namespaces
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@943 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- family.py
+++ family.py
@@ -7,10 +7,63 @@ import config, urllib
 class Family:
     langs = {}
 
-    namespaces = {}
-
-    for i in range(-2,16):
-        namespaces[i]={}
+    # MediaWiki default namespace titles
+    namespaces = {
+        -2: {
+            '_default': u'Media',
+        },
+        -1: {
+            '_default': u'Special',
+        },
+        0: {
+            '_default': None,
+        },
+        1: {
+            '_default': 'Talk',
+        },
+        2: {
+            '_default': u'User',
+        },
+        3: {
+            '_default': u'User talk',
+        },
+        4: {
+            '_default': u'Project',
+        },
+        5: {
+            '_default': u'Project talk',
+        },
+        6: {
+            '_default': u'Image',
+        },
+        7: {
+            '_default': u'Image talk',
+        },
+        8: {
+            '_default': u'MediaWiki',
+        },
+        9: {
+            '_default': u'MediaWiki talk',
+        },
+        10: {
+            '_default':u'Template',
+        },
+        11: {
+            '_default': u'Template talk',
+        },
+        12: {
+            '_default': u'Help',
+        },
+        13: {
+            '_default': u'Help talk',
+        },
+        14: {
+            '_default': u'Category',
+        },
+        15: {
+            '_default': u'Category talk',
+        },
+    }
 
     def _addlang(self, code, location, namespaces):
         """Add a new language to the langs and namespaces of the family.
@@ -20,56 +73,52 @@ class Family:
         for num, val in namespaces.items():
             self.namespaces[num][code]=val
         
-    def namespace(self, code, namespace_number, fallback = 'en'):
-        """Give the name of the name space number namespace_number
-           in the language given by code. If no such namespace is known,
-           use the name in the fallback language (default en:)"""
+    def namespace(self, code, namespace_number, fallback = '_default'):
         if self.namespaces[namespace_number].has_key(code):
             return self.namespaces[namespace_number][code]
         elif fallback:
             return self.namespaces[namespace_number][fallback]
         else:
             raise KeyError('ERROR: title for namespace %d in language %s unknown' % (namespace_number, code))  
-
-    def special_namespace(self, code, fallback = 'en'):
-        # Returns the title of the special namespace in language 'code', taken from
-        # dictionary above.
-        # If the dictionary doesn't contain a translation, it will use language
-        # 'fallback' (English by default).
-        # If you want the bot to crash in case of an unknown namespace name, use
-        # fallback = None.
+    
+    # Returns the title of the special namespace in language 'code', taken from
+    # dictionary above.
+    # If the dictionary doesn't contain a translation, it will use language
+    # 'fallback' (or, if fallback isn't given, MediaWiki default).
+    # If you want the bot to crash in case of an unknown namespace name, use
+    # fallback = None.
+    def special_namespace(self, code, fallback = '_default'):
         return self.namespace(code, -1, fallback)
-
-    def special_namespace_url(self, code, fallback = 'en'):
+    
+    def special_namespace_url(self, code, fallback = '_default'):
         encoded_title = self.namespace(code, -1, fallback).encode(self.code2encoding(code))
         return urllib.quote(encoded_title)
-
-    def image_namespace(self, code, fallback = 'en'):
+    
+    def image_namespace(self, code, fallback = '_default'):
         return self.namespace(code, 6, fallback)
-
-    def image_namespace_url(self, code, fallback = 'en'):
+    
+    def image_namespace_url(self, code, fallback = '_default'):
         encoded_title = self.namespace(code, 6, fallback).encode(self.code2encoding(code))
         return urllib.quote(encoded_title)
-
-    def mediawiki_namespace(self, code, fallback = 'en'):
+    
+    def mediawiki_namespace(self, code, fallback = '_default'):
         return self.namespace(code, 8, fallback)
-
-    def template_namespace(self, code, fallback = 'en'):
+    
+    def template_namespace(self, code, fallback = '_default'):
         return self.namespace(code, 10, fallback)
-
-    def category_namespace(self, code, fallback = 'en'):
+     
+    def category_namespace(self, code, fallback = '_default'):
         return self.namespace(code, 14, fallback)
-
-    # TODO: rewrite (?)
+    
     def category_namespaces(self, code):
         namespaces = []
         namespace_title = self.namespace(code, 14)
         namespaces.append(namespace_title)
         namespaces.append(namespace_title.lower())
-        english_namespace_title = self.namespace('en', 14)
-        if namespace_title != english_namespace_title:
-            namespaces.append(english_namespace_title)
-            namespaces.append(english_namespace_title.lower())
+        default_namespace_title = self.namespace('_default', 14)
+        if namespace_title != default_namespace_title:
+            namespaces.append(default_namespace_title)
+            namespaces.append(default_namespace_title.lower())
         return namespaces
 
     # Redirect code can be translated, but is only in one language now.
