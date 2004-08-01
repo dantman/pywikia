e88220ef852cc662d13d1d55d9ab25bffb1783a5
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-01 14:49:00 +0000

    simplified
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@945 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- meta_family.py
+++ meta_family.py
@@ -10,24 +10,4 @@ import config, urllib, family
 
 class Family(family.Family):
     def __init__(self):
-        self._addlang(code = 'meta',
-                      location = 'meta.wikimedia.org',
-                      namespaces = {-2 : u'Media',
-                                    -1 : u'Special',
-                                    0  : None,
-                                    1 : u'Talk',
-                                    2 : u'User',
-                                    3 : u'User talk',
-                                    4 : u'Wikipedia',
-                                    5 : u'Wikipedia talk',
-                                    6 : u'Image',
-                                    7 : u'Image talk',
-                                    8 : u'MediaWiki',
-                                    9 : u'MediaWiki talk',
-                                    10 : u'Template',
-                                    11 : u'Template talk',
-                                    12 : u'Help',
-                                    13 : u'Help talk',
-                                    14 : u'Category',
-                                    15 : u'Category talk'})
-        
+        self.langs = {'meta': 'meta.wikimedia.org'}
