9d51e33a9e446819e46b56e7a4f6fbb6f7423060
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-01 14:57:08 +0000

    meta uses different titles for the project namespace
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@946 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- meta_family.py
+++ meta_family.py
@@ -11,3 +11,5 @@ import config, urllib, family
 class Family(family.Family):
     def __init__(self):
         self.langs = {'meta': 'meta.wikimedia.org'}
+        self.namespaces[4] = 'Meta'
+        self.namespaces[5] = 'Meta talk'
