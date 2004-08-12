ba4fe241372e612cbe33095453dba09fa024be67
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-12 22:02:09 +0000

    empty file
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1002 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ user-config.py
@@ -0,0 +1,6 @@
+ # add a line here which says: username='xyz' where xyz is your nickname.
+ 
+ 
+ # add a line here which says: mylang='xy' where xy is the language code of the
+ # wiki you're mainly working on, e.g. en for English.
+ 
