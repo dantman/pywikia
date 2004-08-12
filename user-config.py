a8300c9b43083fd751b45773b2a2575db9f19d49
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-12 22:10:32 +0000

    bad idea, removed
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1003 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- user-config.py
+++
@@ -1,6 +0,0 @@
- # add a line here which says: username='xyz' where xyz is your nickname.
- 
- 
- # add a line here which says: mylang='xy' where xy is the language code of the
- # wiki you're mainly working on, e.g. en for English.
- 
