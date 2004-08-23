ea7fd4fac774cdf3ac4490cdb5e530d5777f5d5d
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-08-23 14:33:40 +0000

    Bug repair and removing debug
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1036 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- test.py
+++ test.py
@@ -10,33 +10,13 @@ __version__='$Id$'
 #
 import re,sys,wikipedia
 
-if 0:
-    pl=wikipedia.PageLink('af','1832')
-    print pl.get()
-elif 0:
-    wikipedia.family.langs={'test':'test.wikipedia.org'}
-
-    text=wikipedia.getPage('test','Robottest')
-    text=text+'\nrobot was here\n'
-    status,reason,data=wikipedia.putPage('test','Robottest',text)
-    print status,reason
-elif 1:
-    for arg in sys.argv[1:]:
-        arg = wikipedia.argHandler(arg)
-        if arg:
-            print "Unknown argument",arg
-                sys.exit(1)
+for arg in sys.argv[1:]:
+    arg = wikipedia.argHandler(arg)
+    if arg:
+        print "Unknown argument",arg
+        sys.exit(1)
                 
-    if wikipedia.checkLogin():
-        print "Logged in ("+wikipedia.mylang+".wikipedia.org)"
-    else:
-        print "Not logged in ("+wikipedia.mylang+".wikipedia.org)"
-elif 0:
-    x1='\xb1\xb3\xbf'
-    import codecs
-    encode_func, decode_func, stream_reader, stream_writer = codecs.lookup('iso-8859-2')
-    x2,l=decode_func(x1)
-    print repr(x2)
-
-    u2=wikipedia.link2url(x1,'pl')
-    print u2
+if wikipedia.checkLogin():
+    print "Logged in ("+wikipedia.mylang+".wikipedia.org)"
+else:
+    print "Not logged in ("+wikipedia.mylang+".wikipedia.org)"
