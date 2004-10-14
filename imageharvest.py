9b0f314df090de74f893a1ae75e596cc9a191e4f
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-10-14 14:48:03 +0000

    Another new bot in the first place meant for WikiCommons. This bot needs to get a URL, and then goes through all images on that page, asking whether to upload them to your home wiki.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1131 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ imageharvest.py
@@ -0,0 +1,71 @@
+# -*- coding: iso8859-1 -*-
+"""
+Bot for getting multiple images from an external site. It takes a URL as an
+argument and finds all images (and other files specified by the extensions
+in 'fileformats') that URL is referring to, asking whether to upload them.
+If further arguments are given, they are considered to be the text that is
+common to the descriptions.
+"""
+
+import re, sys, os
+import wikipedia, lib_images
+
+def get_imagelinks(url):
+    # Given a URL, get all images linked to by the page at that URL.
+    # First, we get the location for relative links from the URL.
+    relativepath = url.split("/")
+    if len(relativepath) == 1:
+        relativepath=relativepath[0]
+    else:
+        relativepath=relativepath[:len(relativepath)-1]
+        relativepath="/".join(relativepath)
+    links = []
+    uo = wikipedia.MyURLopener()
+    file = uo.open(url)
+    text = file.read()
+    file.close()
+    text = text.lower()
+    R=re.compile("[(HREF)(href)]=[\"'](.*?)[\"']")
+    for link in R.findall(text):
+        ext = os.path.splitext(link)[1].lower().strip('.')
+        if ext in fileformats:
+            if re.compile("://").match(text):
+                links += [link]
+            else:
+                links += [relativepath+"/"+link]
+    return links
+
+url = ''
+fileformats = ('jpg', 'jpeg', 'png', 'gif', 'svg', 'ogg')
+basicdesc = []
+  
+for arg in sys.argv[1:]:
+    arg = wikipedia.argHandler(arg)
+    if arg:
+        if url == '':
+            url = arg
+        else:
+            desc += [arg]
+
+if not wikipedia.cookies:
+    print "You must be logged in to upload images"
+    import sys
+    sys.exit(1)
+
+if url == '':
+    url = wikipedia.input(u"From what URL should I get the images?")
+
+if basicdesc == []:
+    basicdesc = wikipedia.input(
+        u"What text should be added at the end of the description of each image from this url?")
+else:
+    basicdesc = ' '.join(desc)
+
+ilinks = get_imagelinks(url)
+
+for image in ilinks:
+    if wikipedia.input(u"Include image %s (y/N)?"%image) in ["y","Y"]:
+        desc = wikipedia.input(u"Give the description of this image:")
+        desc = desc + "\r\n\n\r" + basicdesc
+        lib_images.get_image(image, None, desc)
+
