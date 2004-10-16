90f3ee331c775f0085247ebf57020c38e9d517b6
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-10-16 15:17:52 +0000

    cs: wiktionary goes case-sensitive
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1142 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wiktionary_family.py
+++ wiktionary_family.py
@@ -166,7 +166,7 @@ class Family(family.Family):
     # On most Wikipedias page names must start with a capital letter, but some
     # languages don't use this.
         
-    nocapitalize = ['fr','it','nl','tlh','tokipona']
+    nocapitalize = ['cs','fr','it','nl','tlh','tokipona']
 
     # Which languages have a special order for putting interlanguage links,
     # and what order is it? If a language is not in interwiki_putfirst,
