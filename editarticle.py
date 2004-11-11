dc72c1e522463d75d926e02161612154742ba67d
gerrit <gerrit@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-11-11 20:01:03 +0000

    edits under username now possible
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1178 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- editarticle.py
+++ editarticle.py
@@ -5,10 +5,17 @@
 # (C) Gerrit Holl 2004
 # Distribute under the terms of the PSF license.
 
-# Version 0.1.
+# Version 0.2.
 #
-# TODO: - logging in
+# Features:
+#       - logging in
+#       - editing anonymously
+#
+# TODO: - non existing pages
 #       - correct encoding
+#       - use cookies to remember login
+#       - redirect pages
+#
 #       - ...
 #
 
@@ -16,11 +23,14 @@ __version__ = "$Id$"
 
 import sys
 import os
+import httplib
+## import cookielib
 import getpass
 import optparse
 import tempfile
 
 import wikipedia
+import login
 
 def options(args):
     parser = optparse.OptionParser()
@@ -31,18 +41,12 @@ def options(args):
 ##    parser.add_option("-p", "--password", help="Password to login with")
     return parser.parse_args(args=args)
 
-def main():
-    args = []
-    for arg in sys.argv[1:]:
-        arg = wikipedia.argHandler(arg)
-        if arg:
-            args.append(arg)
-    opts, otherargs = options(args)
-    if not opts.anonymous:
-        raise NotImplementedError("logging in is not implemented yet, please use -a")
-    page = opts.page or raw_input("Page to edit: ")
-    editor = opts.editor or raw_input("Editor to use: ")
-    pl = wikipedia.PageLink(wikipedia.getSite(), page)
+def editpage(pl, editor):
+    """Edit a pagelink using an editor.
+    
+    Takes two arguments: page and editor.
+    
+    It returns two strings: the old version and the new version."""
     ofn = tempfile.mktemp()
     ofp = open(ofn, 'w')
     oldcontent = pl.get()
@@ -50,8 +54,58 @@ def main():
     ofp.close()
     os.system("%s %s" % (editor, ofn))
     newcontent = open(ofn).read().decode('utf-8')
-    if oldcontent != newcontent:
-        pl.put(newcontent, comment=raw_input("What did you change? "), anon=True)
+    return oldcontent, newcontent
+
+## def login(username, password):
+##     """Login with username and password.
+##     
+##     Return cookie object on success, False otherwise"""
+## 
+##     data = {"wpName": username,
+##             "wpPassword": password,
+##             "wpLoginattempt": "Aanmelden & inschrijven",
+##             "wpRemember": "0"} # FIXME: cookies
+##     data = wikipedia.urlencode(data.items())
+##     headers = {"Content-type": "application/x-www-form-urlencoded",
+##                "User-agent": "editarticle.py $Revision$"}
+##     mysite = wikipedia.getSite()
+##     pagename = mysite.login_address()
+##     conn = httplib.HTTPConnection(mysite.hostname())
+##     conn.request("POST", pagename, data, headers, )
+##     response = conn.getresponse()
+##     cookie = response.getheader("set-cookie")
+##     print cookie
+##     if response.status == 302: # Moved temporarily
+##         return cookie
+##     else:
+##         return False
+
+def main():
+    args = []
+    for arg in sys.argv[1:]:
+        arg = wikipedia.argHandler(arg)
+        if arg:
+            args.append(arg)
+    opts, otherargs = options(args)
+    if opts.anonymous:
+        site = wikipedia.getSite()
+    else:
+        username = opts.username or raw_input("Username: ")
+        site = wikipedia.getSite(user=opts.username)
+        password = getpass.getpass("Password: ")
+        cookie = login.login(site, username, password)
+        if cookie:
+            login.storecookiedata(cookie, site, username)
+            print "Login succesful"
+        else:
+            sys.exit("Login failed")
+
+    page = opts.page or raw_input("Page to edit: ")
+    editor = opts.editor or raw_input("Editor to use: ")
+    pl = wikipedia.PageLink(site, page)
+    old, new = editpage(pl, editor)
+    if old != new:
+        pl.put(new, comment=raw_input("What did you change? "), anon=opts.anonymous)
     else:
         print "Nothing changed"
 
