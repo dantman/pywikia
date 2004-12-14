6b0bb86db37f000b3740639b8413f1de8966b40b
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2004-12-14 12:32:56 +0000

    ja: uncapitalized
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1265 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wiktionary_family.py
+++ wiktionary_family.py
@@ -201,7 +201,7 @@ class Family(family.Family):
         
     # group of languages that we might want to do at once
     
-    cyrilliclangs = ['be', 'bg', 'mk', 'ru', 'sr', 'uk'] # languages in Cyrillic
+    cyrilliclangs = ['be', 'bg', 'ja', 'mk', 'ru', 'sr', 'uk'] # languages in Cyrillic
        
     # Function to figure out the encoding of different languages
     # Languages that are coded in iso-8859-1
