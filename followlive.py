63c769456657cb8d3b2fc826305a21e15aa61368
gerrit <gerrit@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-02-23 21:22:24 +0000

    flush stdout
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1415 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- followlive.py
+++ followlive.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
+import sys
 import wikipedia
 
 def main():
     for pageinfo in wikipedia.newpages():
+        sys.stdout.flush()
         print pageinfo["date"]
         print pageinfo["title"]
         print "Length:", pageinfo["length"],
+        sys.stdout.flush()
         if pageinfo["length"] < 200 and pageinfo.get("user_anon"):
             print "SPAM SUSPECTED!"
             print
@@ -19,6 +22,7 @@ def main():
         print pageinfo.get("user_login") or pageinfo.get("user_anon")
         print pageinfo.get("comment") or "no comment"
         print '---'
+        sys.stdout.flush()
 if __name__ == "__main__":
     try:
         main()
