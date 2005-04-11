4459726c13a2f4fb2f86590e0e8ecd38930a17ca
a_engels <a_engels@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-04-11 11:37:45 +0000

    option -bare to give the links as [[Title]] instead of [[family:ln:Title]]
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1563 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- extract_wikilinks.py
+++ extract_wikilinks.py
@@ -8,6 +8,7 @@ This script takes a single file name argument, the file should be a HTML file
 as captured from one of the wikipedia servers.
 
 Arguments:
+-bare       Extract as internal links: [[Title]] instead of [[Family:xx:Title]]
 -sorted     Print the pages sorted alphabetically (default: the order in which
             they occur in the HTML file)
 """
@@ -18,18 +19,21 @@ Arguments:
 #
 __version__='$Id$'
 #
-import sys,re,wikipedia
+import sys,re,wikipedia,codecs
 wikipedia.stopme() # This bot does not contact the Wiki, so no need to get it on the list 
 R = re.compile('/wiki/(.*?)" *')
 fn = []
 sorted = False
 list = []
+complete = True
 
 for arg in sys.argv[1:]:
     arg = wikipedia.argHandler(arg)
     if arg:
-        if arg=="-sorted":
+        if arg.startswith("-sorted"):
             sorted = True
+        elif arg.startswith("-bare"):
+            complete = False
         elif fn:
             print "Ignoring argument %s"%arg
         else:
@@ -41,11 +45,14 @@ if not fn:
 
 mysite = wikipedia.getSite()
 
-f=open(fn)
+f=open(fn,'r')
 text=f.read()
 f.close()
 for hit in R.findall(text):
-    list += [mysite.linkto(hit)]
+    if complete:
+        list.append(mysite.linkto(hit))
+    else:
+        list.append("[[%s]]"%hit)
 if sorted:
     list.sort()
 for page in list:
