f2e938da1786bd731ba5daab5eba892cf7292186
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-05-06 20:07:26 +0000

    added sv:
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1609 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/memoryalpha_family.py
+++ families/memoryalpha_family.py
@@ -14,6 +14,7 @@ class Family(family.Family):
             'de':'de',
             'en':'en',
             'nl':'nl',
+            'sv':'sv',
             }
     
         # Most namespaces are inherited from family.Family.
