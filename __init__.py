f47dfb6fec587719e6c14a7472db1020f2580946
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-06-21 14:21:41 +0000

    Documentation
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1750 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ __init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8  -*-
+#
+# pywikipediabot
+#
+# Python Wikipedia Robot Framework
+#
+
+"""
+The Python Wikipedia Robot Framework is a set of bots to do things on Wikipedia or other
+Mediawiki projects. It has been written by several people using the language Python.
+
+@license: Python Software Foundation
+@copyright: (C) 2003 Rob W.W. Hooft.
+
+@Operating System:
+    All 32-bit MS Windows (95/98/NT/2000/XP),
+    All POSIX (Linux/BSD/UNIX-like OSes)
+
+@contributors see http://sourceforge.net/project/memberlist.php?group_id=93107
+@documentation see http://pywikipediabot.v10.com.br
+
+"""
+__docformat__ = 'epytext en'
+
+# General info
+__version__ = '1.0'
+__author__ = 'Rob W.W. Hooft <rob@hooft.net>'
+__url__ = 'http://pywikipediabot.sourceforge.net'
+__license__ = 'Python Software Foundation'
