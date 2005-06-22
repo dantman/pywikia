c86cc55a9bde1f4d5e6557c35c3ac8938d022b56
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-06-22 08:30:41 +0000

    feature: touch all pages listed in a textfile
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1760 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- touch.py
+++ touch.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 
+"""
+This bot goes over multiple pages of the home wiki, and edits them without
+changing. This is for example used to get category links in templates
+working.
+
+This script understands various command-line arguments:
+
+    -start:        used as -start:pagename, specifies that the robot should
+                   go alphabetically through all pages on the home wiki,
+                   starting at the named page.
+
+    -file:         used as -file:filename, read a list of pages to treat
+                   from the named textfile. Page titles should be enclosed
+                   in [[double-squared brackets]].
+
+    -ref:          used as -start:pagename, specifies that the robot should
+                   touch all pages referring to the named page.
+
+All other parameters will be regarded as a page title; in this case, the bot
+will only touch a single page.
+"""
 import wikipedia, pagegenerators
 import sys
 
@@ -32,13 +53,15 @@ def main():
             elif arg.startswith('-ref:'):
                 referredPage = wikipedia.Page(wikipedia.getSite(), arg[5:])
                 gen = pagegenerators.ReferringPageGenerator(referredPage)
+            elif arg.startswith('-file:'):
+                gen = pagegenerators.TextfilePageGenerator(arg[6:])
             else:
                 pageTitle.append(arg)
-                
+
     if pageTitle:
         page = wikipedia.Page(wikipedia.getSite(), ' '.join(pageTitle))
         gen = pagegenerators.SinglePageGenerator(page)
-    
+
     preloadingGen = pagegenerators.PreloadingGenerator(gen)
     bot = TouchBot(preloadingGen)
     bot.run()
