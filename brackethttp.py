7c14ea41f5fa269df7bdba8cc8cabc79db14b423
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-06-22 13:05:22 +0000

    *** empty log message ***
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1767 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- brackethttp.py
+++ brackethttp.py
@@ -1,41 +1,38 @@
-"""
+'''
 Script to correct URLs like
 (http://www.example.org) to [http://www.example.org example.org]
 to have correct generation of links in Wikipedia
-"""
-#
-# (C) 2003 Thomas R. Koll, <tomk32@tomk32.de>
-#
-# Distribute under the terms of the PSF license.
-#
-__version__='$Id$'
-#
-import re, sys
+'''
 
+__author__ = '(C) 2003 Thomas R. Koll, <tomk32@tomk32.de>'
+__license__ = 'Distribute under the terms of the PSF license.'
+__version__='$Id$'
+
+import re, sys
 import wikipedia
 
 myComment = {'en':'Bot: URL fixed',
-             'pt':'Bot: Corrigindo URL'
+             'pt':'Bot: URL corrigido'
              }
 
-try:
-    for arg in sys.argv[1:]:
-        if wikipedia.argHandler(arg, 'brackethttp'):
-            pass
-        else:
-            pl = wikipedia.Page(wikipedia.getSite(), arg)
-            text = pl.get()
-        
-            newText = re.sub("(http:\/\/([^ ]*[^\] ]))\)", "[\\1 \\2])", text)
-
-            if newText != text:
-                wikipedia.showDiff(text, newText)
-                status, reason, data = pl.put(newText, wikipedia.translate(wikipedia.mylang,myComment))
-                print status, reason
+if __name__ == "__main__":
+    try:
+        for arg in sys.argv[1:]:
+            if wikipedia.argHandler(arg, 'brackethttp'):
+                pass
             else:
-                print "No bad link found"
-except:
-    wikipedia.stopme()
-    raise
+                pl = wikipedia.Page(wikipedia.getSite(), arg)
+                text = pl.get()
+        
+                newText = re.sub("(http:\/\/([^ ]*[^\] ]))\)", "[\\1 \\2])", text)
 
+                if newText != text:
+                    wikipedia.showDiff(text, newText)
+                    status, reason, data = pl.put(newText, wikipedia.translate(wikipedia.mylang,myComment))
+                    print status, reason
+                else:
+                    print "No bad link found"
+    except:
+        wikipedia.stopme()
+        raise
 wikipedia.stopme()
