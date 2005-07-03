66aa42fd0e4728efcdb257b9b5d47fb2f4567c7b
hooft <hooft@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-07-03 05:37:15 +0000

    * Top class  * Use __call__ special function
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1819 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- pagegenerators.py
+++ pagegenerators.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 
+# Standard library imports
 import re, codecs
+
+# Application specific imports
 import wikipedia
 
-class SinglePageGenerator:
+class PageGenerator(object):
+    def __call__(self):
+        return self.generate()
+
+    def generate(self):
+        raise Exception("Abstract class")
+    
+class SinglePageGenerator(PageGenerator):
     '''
     Pseudo-generator
     '''
@@ -14,7 +24,7 @@ class SinglePageGenerator:
     def generate(self):
         yield self.page
 
-class AllpagesPageGenerator:
+class AllpagesPageGenerator(PageGenerator):
     '''
     Using the Allpages special page, retrieves all articles, loads them (60 at
     a time) using XML export, and yields title/text pairs.
@@ -26,7 +36,7 @@ class AllpagesPageGenerator:
         for page in wikipedia.allpages(start = self.start):
             yield page
 
-class ReferringPageGenerator:
+class ReferringPageGenerator(PageGenerator):
     '''
     Yields all pages referring to a specific page.
     '''
@@ -38,7 +48,7 @@ class ReferringPageGenerator:
         for page in self.referredPage.getReferences(follow_redirects = self.followRedirects):
             yield page
 
-class CategorizedPageGenerator:
+class CategorizedPageGenerator(PageGenerator):
     '''
     Yields all pages in a specific category.
     '''
@@ -50,7 +60,7 @@ class CategorizedPageGenerator:
         for page in self.category.articles(recurse = self.recurse):
             yield page
 
-class LinkedPageGenerator:
+class LinkedPageGenerator(PageGenerator):
     '''
     Yields all pages linked from a specific page.
     '''
@@ -61,7 +71,7 @@ class LinkedPageGenerator:
         for page in self.linkingPage.linkedPages():
             yield page
 
-class TextfilePageGenerator:
+class TextfilePageGenerator(PageGenerator):
     '''
     Read a file of page links between double-square-brackets, and return
     them as a list of Page objects. 'filename' is the name of the file that
@@ -92,13 +102,13 @@ class TextfilePageGenerator:
             yield wikipedia.Page(site, pagename)
         f.close()
 
-class PreloadingGenerator:
+class PreloadingGenerator(PageGenerator):
     """
     Wraps around another generator. Retrieves as many pages as stated by pageNumber
     from that generator, loads them using Special:Export, and yields them one after
     the other. Then retrieves more pages, etc.
     """
-    def __init__(self, generator, pageNumber = 60):
+    def __init__(self, generator, pageNumber=60):
         self.generator = generator
         self.pageNumber = pageNumber
 
@@ -114,11 +124,11 @@ class PreloadingGenerator:
         # after these pages have been preloaded.
         somePages = []
         i = 0
-        for page in self.generator.generate():
+        for page in self.generator():
             i += 1
             somePages.append(page)
             # We don't want to load too many pages at once using XML export.
-            # We only get 20 at a time.
+            # We only get a maximum number at a time.
             if i >= self.pageNumber:
                 self.preload(somePages)
                 for refpage in somePages:
