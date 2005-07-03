1fc228d714a9d21bf88b9e7559614768ff2d2a81
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-07-03 22:11:03 +0000

    still far from being useful
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@1833 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- userinterfaces/tkinter_interface.py
+++ userinterfaces/tkinter_interface.py
@@ -22,13 +22,40 @@ class CustomMessageBox(tkMessageBox.Message):
         return self.selection
 
 class UI:
-    def __init__(self):
-        pass
+    def __init__(self, parent = None):
+        # create a new window if necessary
+        self.parent = parent or Tk()
 
-    def output(self, text, newline = True):
+        self.top_frame = Frame(parent)
+
+        scrollbar = Scrollbar(self.top_frame)
+        # textarea with vertical scrollbar
+        self.logBox = Text(self.top_frame, yscrollcommand=scrollbar.set)
+        # add scrollbar to main frame, associate it with our editbox
+        scrollbar.pack(side=RIGHT, fill=Y)
+        scrollbar.config(command=self.logBox.yview)
+        # put textarea into top frame, using all available space
+        self.logBox.pack(anchor=CENTER, fill=BOTH)
+        self.top_frame.pack(side=TOP)
+
+    def output(self, text, urgency = 1, newline = True):
         """
+        urgency levels:
+            0 - Debug output. Won't be shown in normal mode.
+            1 - Will be shown in log window.
+            2 - Will be shown in error box.
+
+            TODO: introduce constants
         """
-        d = tkMessageBox.showinfo('title', text)
+        if urgency >= 2:
+            d = tkMessageBox.showinfo('title', text)
+        if urgency >= 1:
+            self.logBox.insert(END, text)
+            if newline:
+                self.logBox.insert(END, '\n')
+            # auto-scroll down
+            self.logBox.see(END)
+            d = tkMessageBox.showinfo('title', text)
 
     def input(self, question):
         """
@@ -36,18 +63,16 @@ class UI:
         """
 
         answer = tkSimpleDialog.askstring('title', question)
-        return text
+        return answer
 
     def inputChoice(self, question, options, hotkeys):
-        root = Tk()
-        #Button(root, text="Hello!").pack()
-        #root.update()
-        
-        d = CustomMessageBox(root, question, options, hotkeys)
-        root.wait_window(d.top)
+
+        d = CustomMessageBox(self.root, question, options, hotkeys)
+        self.root.wait_window(d.top)
         answer = d.ask()
         return answer
 
-ui = UI()
-ui.output('Test')
-print ui.inputChoice('Test?', ['foo', 'bar'], ['f', 'b'])
+if __name__ == "__main__":
+    ui = UI()
+    ui.output('Test')
+    print ui.inputChoice('Test?', ['foo', 'bar'], ['f', 'b'])
