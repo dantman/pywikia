61a5a438e8fdf2e0c6c67778b9a254c7d57dd4dd
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-09-01 18:26:22 +0000

    only one desambig macro from pt
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2151 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- family.py
+++ family.py
@@ -518,7 +518,7 @@ class Family:
             'nl'  :       [u'dp','dP'],
             'nn'  :       [u'fleirtyding'],
             'pl'  :       [u'disambig',u'disambRulers',u'disambigC'],
-            'pt'  :       [u'desambiguação', u'desamb', u'minidesambig', u'desambigexplicada', u'Ver desambiguação', u'Ver desambiguação1', u'desambiguação-redirect'],
+            'pt'  :       [u'desambiguação'],
             'ro'  :       [u'dezambiguizare'],
             'ru'  :       [u'disambig',u'значения'],
             'simple':     [u'disambig', u'disambiguation'],
