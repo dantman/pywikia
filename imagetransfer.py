01c0c8e1e440ed11a8c5eb7166990d185a73086d
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2005-12-23 17:35:51 +0000

    bugfix: only add {{NowCommons}} to original image page
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2294 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- imagetransfer.py
+++ imagetransfer.py
@@ -139,7 +139,7 @@ class ImageTransferBot:
                 if nowCommonsTemplate.has_key(sourceSite.lang) and config.usernames.has_key(sourceSite.family.name) and config.usernames[sourceSite.family.name].has_key(sourceSite.lang):
                 # add the nowCommons template.
                     wikipedia.output(u'Adding nowCommons template to %s' % sourceImagePage.title())
-                    sourceImagePage.put(description + '\n\n' + nowCommonsTemplate[sourceSite.lang] % targetFilename, comment = nowCommonsMessage[sourceSite.lang])
+                    sourceImagePage.put(sourceImagePage.get() + '\n\n' + nowCommonsTemplate[sourceSite.lang] % targetFilename, comment = nowCommonsMessage[sourceSite.lang])
 
     def showImageList(self, imagelist):
         for i in range(len(imagelist)):
