8b10a1f2e4517978b81b2067e8f9ab0c4f849ddd
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-01-04 12:23:54 +0000

    bugfix
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2306 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -2580,7 +2580,7 @@ class Site(object):
         if getagain or (getalways and ((sysop and not self._sysoptoken) or (not sysop and not self._token))):
             output(u"Getting page to get a token.")
             try:
-                Page(self, "Wikipedia:Sandbox").get(force = True, sysop = sysop)
+                Page(self, "%s:Sandbox" % self.family.namespace(self.lang, 4)).get(force = True, sysop = sysop)
             except UserBlocked:
                 raise
             except Error:
