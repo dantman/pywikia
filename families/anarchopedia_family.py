19872c0f010b235175bec2ec51b4e8b87b298cd3
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-05-06 15:49:13 +0000

    anarchopedia family
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2607 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ families/anarchopedia_family.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8  -*-
+import family
+
+# The Anarchopedia family
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'anarchopedia'
+
+        for lang in self.knownlanguages:
+            self.langs[lang] = lang+'.anarchopedia.org'
+
+        self.namespaces[4] = {
+            '_default': [u'Anarchopedia'],
+        }
+        self.namespaces[5] = {
+            '_default': [u'Anarchopedia talk'],
+        }
+    
+        alphabetic = ['ar','id','da', 'de', 'es', 'eo', 'fr', 'hr', 'it',
+                      'nl', 'no', 'nn', 'pl', 'pt', 'en', 'ru', 'sr', 'sv']
+            
+    def version(self, code):
+        return "1.6alpha"
+
+    def path(self, code):
+        return '/index.php'
