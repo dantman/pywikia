3cd83673b0bbda6b905b19d7af2d2986f5e8f6fe
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-05-23 14:02:24 +0000

    memoryalpha_family patches [ 1492088 ]
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2613 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/memoryalpha_family.py
+++ families/memoryalpha_family.py
@@ -11,27 +11,60 @@ class Family(family.Family):
         self.langs = {
             'de': None,
             'en': None,
+            'eo': None,
+            'es': None,
+            'fr': None,
             'nl': None,
+            'pl': None,
             'sv': None,
             }
+
+        # Override defaults
+        self.namespaces[2]['pl'] = u'Użytkownik'
+        self.namespaces[3]['pl'] = u'Dyskusja użytkownika'
     
         # Most namespaces are inherited from family.Family.
+		
         self.namespaces[4] = {
-            '_default': [u'Memory Alpha', self.namespaces[4]['_default']],
+            '_default': u'Memory Alpha',
         }
         self.namespaces[5] = {
-            '_default': [u'Memory Alpha talk', self.namespaces[5]['_default']],
+            '_default': u'Memory Alpha talk',
             'de': u'Memory Alpha Diskussion',
+            'eo': u'Memory Alpha diskuto',
+            'es': u'Memory Alpha Discusión',
+            'fr': u'Discussion Memory Alpha',
             'nl': u'Overleg Memory Alpha',
+            'pl': u'Dyskusja Memory Alpha',
             'sv': u'Memory Alphadiskussion',
         }
+        self.namespaces[100] = {
+			'_default': u'Forum',
+		}
+        self.namespaces[101] = {
+			'_default': u'Forum talk',
+			'de': u'Forum Diskussion',
+		}
         
         # A few selected big languages for things that we do not want to loop over
         # all languages. This is only needed by the titletranslate.py module, so
         # if you carefully avoid the options, you could get away without these
         # for another wiki family.
-        self.languages_by_size = ['en', 'de', 'nl', 'sv']
+        self.languages_by_size = ['en', 'de', 'es', 'nl', 'sv', 'fr', 'eo', 'pl']
+		
+        alphabetic = ['de', 'en', 'es', 'eo', 'fr', 'nl', 'pl', 'sv']
 
+        self.mainpages = {
+            'de' :			u'Hauptseite',
+            'en' :			u'Main Page',
+            'eo' :			u'Ĉefpaĝo',
+            'es' :			u'Portada',
+            'fr' :			u'Accueil',
+            'nl' :			u'Hoofdpagina',
+            'pl' :			u'Strona główna',
+            'sv' :			u'Huvudsida',
+		}
+		
     def hostname(self,code):
         return 'www.memory-alpha.org'
 
@@ -39,4 +72,4 @@ class Family(family.Family):
         return '/%s/index.php' % code
 
     def version(self, code):
-        return "1.4"
+        return "1.7alpha"
