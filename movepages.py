9b5086eaa68b0f6a114aa76fae46b25657b77e32
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-06-20 20:36:10 +0000

    documentation, work on -ref, -cat and single pages
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2686 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- movepages.py
+++ movepages.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 """
-Bot page moves to another title in category.
+Bot page moves to another title. Special Wikibooks-like pages.
+
+Command-line arguments:
+
+    -cat           Work on all pages which are in a specific category.
+                   Argument can also be given as "-cat:categoryname".
+
+    -ref           Work on all pages that link to a certain page.
+                   Argument can also be given as "-ref:referredpagetitle".
+
+Single pages use: movepages.py Helen_Keller
 
 """
+#
+# (C) Leonardo Gregianin, 2006
 #
 # Distributed under the terms of the MIT license.
 #
 
+__version__='$Id$'
+
 import wikipedia, pagegenerators, catlib
+import sys
 
 comment={
     'en': u'Pagemove by bot',
@@ -17,25 +32,61 @@ comment={
 
 def Movepages(page):
     try:
-        wikipedia.output(u'\n>>> %s <<<<' % page.title())
+        mysite = wikipedia.getSite()
         pagetitle = page.title()
+        wikipedia.output(u'\n>>>> %s <<<<' % pagetitle)
         pagemove = wikipedia.input(u'Another title:')
         titleroot = wikipedia.Page(mysite, pagetitle)
-
         msg = wikipedia.translate(mysite, comment)
         titleroot.move(pagemove, msg)
+
+        # if titleroot don't have referred pages, delete.
+        # pagedel = wikipedia.Page(mysite, pagetitle)
+        # pagedel.delete(pagetitle)
+        
     except:
         pass #ctrl-c: next page
 
 def main():
     mysite = wikipedia.getSite()
-    categoryname = wikipedia.input(u'Please enter the category name:')
-    cat = catlib.Category(mysite, 'Category:%s' % categoryname)
-    gen = pagegenerators.CategorizedPageGenerator(cat)
-    generator = pagegenerators.PreloadingGenerator(gen, pageNumber = [])
-        
-    for page in generator:
-        Movepages(page)
+    categoryName = None
+    singlePageTitle = []
+    referredPageTitle = None
+    preTitle = None
+    
+    for arg in sys.argv[1:]:
+        arg = wikipedia.argHandler(arg, 'movepages')
+        if arg:
+            if arg.startswith('-cat'):
+                if len(arg) == 4:
+                    categoryName = wikipedia.input(u'Enter the category name:')
+                else:
+                    categoryName = arg[5:]
+            elif arg.startswith('-ref'):
+                if len(arg) == 4:
+                    referredPageTitle = wikipedia.input(u'Links to which page should be processed?')
+                else:
+                    referredPageTitle = arg[5:]
+            else:
+                singlePageTitle.append(arg)
+
+    if categoryName:
+        cat = catlib.Category(mysite, 'Category:%s' % categoryName)
+        gen = pagegenerators.CategorizedPageGenerator(cat)
+        generator = pagegenerators.PreloadingGenerator(gen, pageNumber = [])
+        for page in generator: Movepages(page)
+
+    elif referredPageTitle:
+        referredPage = wikipedia.Page(mysite, referredPageTitle)
+        generator = pagegenerators.ReferringPageGenerator(referredPage)
+        for page in generator: Movepages(page)
+
+    else:
+        singlePageTitle = ' '.join(singlePageTitle)
+        if not singlePageTitle:
+            singlePageTitle = wikipedia.input(u'Which page to move:')
+        singlePage = wikipedia.Page(mysite, singlePageTitle)
+        Movepages(singlePage)
 
 if __name__ == '__main__':
     try:
