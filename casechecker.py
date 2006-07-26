54688b1039c6e2cdaefd4cf1478b3cfff1fe92f6
yurochek <yurochek@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-07-26 10:02:45 +0000

    * Initial release: The bot searches for any pages that have mixed latin & cyrilic letters next to each other in the title or in any of the links.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2733 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ casechecker.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+""" Script to enumerate all pages in the wikipedia and find all titles
+with mixed latin and cyrilic alphabets.
+"""
+
+import sys, query, wikipedia, re
+
+apfrom = ''
+aplimit = 500
+apnamespace = 0
+links = False
+
+cyrLtr = u'[а-яА-Я]'
+#cyrLtr = u'[АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя]'
+latLtr = u'[a-zA-Z]'
+pattern = re.compile(u'(%s%s|%s%s)' % (latLtr, cyrLtr, cyrLtr, latLtr) )
+apfrom = None
+title = None
+
+for arg in sys.argv[1:]:
+    arg = wikipedia.argHandler(arg, 'casechecker')
+    if arg:
+        if arg.startswith('-from:'):
+            apfrom = arg[6:]
+        elif arg.startswith('-limit:'):
+            aplimit = int(arg[7:])
+        elif arg.startswith('-ns:'):
+            apnamespace = int(arg[4:])
+        elif arg == '-links':
+            links = True
+    
+try:
+    while True:
+    
+        params = {'apnamespace' : apnamespace, 
+                  'aplimit' : aplimit, 
+                  'apfrom' : apfrom,
+                  'apfilterredir' : 'nonredirects'}
+        if links:
+            params['what'] = 'allpages|links';
+        else:
+            params['what'] = 'allpages';
+
+        data = query.GetData( wikipedia.getSite().lang, params)
+        try:
+            apfrom = data['query']['allpages']['next']
+        except:
+            apfrom = None
+    
+    
+        for pageID, page in data['pages'].iteritems():
+            title = page['title']
+            m = pattern.search(title)
+            if m is not None:
+                wikipedia.output(u'%d: [[%s]]' % (m.span()[0]+2, title))
+                
+            if links:
+                if 'links' in page:
+                    for l in page['links']:
+                        m = pattern.search(l['*'])
+                        if m is not None:
+                            wikipedia.output(u'  [[%s]]: %d link to [[%s]]' % (title, m.span()[0]+2, l['*']))
+    
+        if apfrom is None:
+            break
+        
+except:
+    if apfrom is not None:
+        wikipedia.output(u'Exception at Title = %s, Next = %s' % (title, apfrom))
+    wikipedia.stopme()
+    raise
---
+++ query.py
@@ -0,0 +1,166 @@
+import wikipedia
+import simplejson
+import urllib
+
+def GetData( lang, params ):
+    """Get data from the query api, and convert it into a data object
+    """
+    site = wikipedia.getSite( lang )
+    
+    for k,v in params.iteritems():
+        if not IsString(v):
+            params[k] = unicode(v)
+
+    params['format'] = 'json'
+    params['noprofile'] = ''
+    
+    for k,v in params.iteritems():
+        if type(v) == type(u''):
+            params[k] = ToUtf8(v)
+
+    path = u"/w/query.php?" + wikipedia.urlencode( params.iteritems() )
+    # wikipedia.output( u"Requesting %s:%s" % (lang, path) )
+    
+    url = site.family.querypath(lang)
+    
+    jsontext = site.getUrl( path, True )
+
+
+    # This will also work, but all unicode strings will need to be converted from \u notation
+    # return eval( jsontext )
+
+    return simplejson.loads( jsontext )
+
+def GetInterwikies( lang, titles, extraParams = None ):
+    """ Usage example: data = GetInterwikies('ru','user:yurik')
+    titles may be either ane title (as a string), or a list of strings
+    extraParams if given must be a dict() as taken by GetData()
+    """
+    
+    params = {'titles':ListToParam(titles), 'what' : 'redirects|langlinks'}
+    params = CombineParams( params, extraParams )
+    return GetData( lang, params )
+
+def GetLinks( lang, titles, extraParams = None ):
+    """ Get list of templates for the given titles
+    """
+    params = {'titles':ListToParam(titles), 'what': 'redirects|links'}
+    params = CombineParams( params, extraParams )
+    return GetData( lang, params )
+
+def GetDisambigTemplates(lang):
+    """This method will return a set of disambiguation templates.
+    Template:Disambig is always assumed to be default, and will be
+    appended (in localized format) regardless of its existence.
+    The rest will be aquired from the Wikipedia:Disambiguation Templates page.
+    Only links to templates will be used from that page.
+    """
+    
+    disambigs = set()
+    disambigName = u"template:disambig"
+    disListName = u"Wikipedia:Disambiguation Templates"
+    disListId = 0
+    
+    templateNames = GetLinks(lang, [disListName, disambigName])
+    for id, page in templateNames['pages'].iteritems():
+        if page['title'] == disambigName:
+            if 'normalizedTitle' in page:
+                disambigs.add(page['normalizedTitle'])
+            elif 'redirect' in page:
+                disambigs.add(page['title'])
+        elif page['title'] == disListName:
+            if 'normalizedTitle' in page:
+                if 'refid' in page:
+                    disListId = page['refid']
+            else:
+                disListId = id
+    
+    # Disambig page was found
+    if disListId > 0:
+        page = templateNames['pages'][disListId]
+        if 'links' in page:
+            for l in page['links']:
+                if l['ns'] == 10:
+                    disambigs.add(l['*'])
+    
+    return disambigs
+#
+#
+# Helper utilities
+#
+#
+
+def CleanParams( params ):
+    """Params may be either a tuple, a list of tuples or a dictionary.
+    This method will convert it into a dictionary
+    """
+    if params is None:
+        return dict()
+    pt = type( params )
+    if pt == type( {} ):
+        return params
+    elif pt == type( () ):
+        if len( params ) != 2: raise u"Tuple size must be 2"
+        return {params[0]:params[1]}
+    elif pt == type( [] ):
+        for p in params:
+            if p != type( () ) or len( p ) != 2: raise u"Every list element must be a 2 item tuple"
+        return dict( params )
+    else:
+        raise u"Unknown param type %s" % pt
+    
+def CombineParams( params1, params2 ):
+    """Merge two dictionaries. If they have the same keys, their values will
+    be appended one after another separated by the '|' symbol.
+    """
+
+    params1 = CleanParams( params1 )
+    if params2 is None:
+        return params1
+    params2 = CleanParams( params2 )
+    
+    for k, v2 in params2.iteritems():
+        if k in params1:
+            v1 = params1[k]
+            if len( v1 ) == 0:
+                params1[k] = v2
+            elif len( v2 ) > 0:
+                if type('') != type(v1) or type('') != type(v2):
+                    raise u"Both merged values must be of type 'str'"
+                params1[k] = v1 + '|' + v2
+            # else ignore
+        else:
+            params1[k] = v2
+    return params1
+
+def ConvToList( item ):
+    """Ensure the output is a list
+    """
+    if item is None:
+        return []
+    elif IsString(item):
+        return [item]
+    else:
+        return item
+
+def ListToParam( list ):
+    """Convert a list of unicode strings into a UTF8 string separated by the '|' symbols
+    """
+    list = ConvToList( list )
+    if len(list) == 0:
+        return ''
+    
+    encList = ''
+    # items may not have one symbol - '|'
+    for l in list:
+        if '|' in l: raise u"item '%s' contains '|' symbol" % l
+        encList += ToUtf8(l) + '|'
+    return encList[:-1]
+
+def ToUtf8(s):
+    if type(s) != type(u''):
+        raise u"item %s is not unicode" % unicode(s)
+    return s.encode('utf-8')
+
+def IsString(s):
+    return type( s ) in [type( '' ), type( u'' )]
