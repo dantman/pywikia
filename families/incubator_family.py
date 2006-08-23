1696764f67ee1af91aa09a6d9e560cad4a42785b
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-08-23 12:37:22 +0000

    creating Incubator Wikimedia family
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2764 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ families/incubator_family.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8  -*-
+
+__version__ = '$Id$'
+
+import family
+
+# The Wikimedia Incubator family
+
+class Family(family.Family):
+    
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'incubator'
+        self.langs = {
+            'incubator': 'incubator.wikimedia.org',
+        }
+        
+        self.namespaces[4] = {
+            '_default': [u'Incubator', self.namespaces[4]['_default']],
+        }
+        self.namespaces[5] = {
+            '_default': [u'Incubator talk', self.namespaces[5]['_default']],
+        }
+
+    def version(self, code):
+        return "1.5"
+
