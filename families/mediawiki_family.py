31e4433358ff9a66ab9778fee543fbdd3823771f
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-10-25 12:45:24 +0000

    create mediawiki family
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@2881 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ families/mediawiki_family.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8  -*-
+
+__version__ = '$Id$'
+
+import family
+
+# The MediaWiki family
+# user-config.py: usernames['mediawiki']['mediawiki'] = 'User name'
+
+
+class Family(family.Family):
+    def __init__(self):
+
+        family.Family.__init__(self)
+        self.name = 'mediawiki'
+
+        self.langs = {
+            'mediawiki': 'www.mediawiki.org',
+        }
+        
+        self.namespaces[4] = {
+            '_default': [u'Project', self.namespaces[4]['_default']],
+        }
+        self.namespaces[5] = {
+            '_default': [u'Project talk', self.namespaces[5]['_default']],
+        }
