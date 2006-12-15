40cf8e975c2f2ac334c25284545e81105d203315
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2006-12-15 23:24:52 +0000

    made object-oriented refactoring don't change if sandbox is still clean fixed some minor bugs added de: message
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3058 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- clean_sandbox.py
+++ clean_sandbox.py
@@ -11,49 +11,59 @@ import wikipedia
 import time
 
 content = {
-    'de': u'{{Bitte erst NACH dieser Zeile schreiben! (Begrüßungskasten)}}\n',
+    'de': u'{{Bitte erst NACH dieser Zeile schreiben! (Begrüßungskasten)}}\r\n',
     'en': u'{{Please leave this line alone (sandbox heading)}}\n <!-- Hello! Feel free to try your formatting and editing skills below this line. As this page is for editing experiments, this page will automatically be cleaned every 12 hours. -->',
-    'pt': u'<!--não apague esta linha-->{{página de testes}}<!--não apagar-->\n',
+    'pt': u'<!--não apague esta linha-->{{página de testes}}<!--não apagar-->\r\n',
     }
 
 msg = {
-    'de': u'Bot: reset',
+    'de': u'Bot: Setze Seite zurück.',
     'en': u'Robot: This page will automatically be cleaned.',
     'pt': u'Bot: Limpeza da página de testes',
     }
     
-page = {
+sandboxTitle = {
     'de': u'Wikipedia:Spielwiese',
     'en': u'Wikipedia:Sandbox',
     'pt': u'Wikipedia:Página de testes',
     }
 
-def SandboxBot(hours):
-    seg = hours*3600
-    mysite = wikipedia.getSite()
-    now = time.strftime("%d %b %Y %H:%M:%S (UTC)", time.gmtime())
-    page_translate = wikipedia.translate(mysite, page)
-    title = wikipedia.Page(mysite, page_translate)
-    try:
-        wikipedia.output(u'\nSleeping %s hours, now %s' % (hours, now))
-        time.sleep(seg)
-        msg_translate = wikipedia.translate(mysite, msg)
-        content_translate = wikipedia.translate(mysite, content)
-        title.put(content_translate, msg_translate)
-        SandboxBot(hours)
-    except wikipedia.EditConflict:
-        wikipedia.output(u'*** Loading again because of edit conflict.\n')
-        wikipedia.output(u'\nSleeping %s hours, now %s' % (hours, now))
-        time.sleep(seg)
-        SandboxBot(hours)
+class SandboxBot:
+    def __init__(self, hours):
+        self.hours = hours
     
+    def run(self):
+        mySite = wikipedia.getSite()
+        while True:
+            now = time.strftime("%d %b %Y %H:%M:%S (UTC)", time.gmtime())
+            localSandboxTitle = wikipedia.translate(mySite, sandboxTitle)
+            sandboxPage = wikipedia.Page(mySite, localSandboxTitle)
+            try:
+                text = sandboxPage.get()
+                translatedContent = wikipedia.translate(mySite, content)
+                if text.strip() == translatedContent.strip():
+                    wikipedia.output(u'The sandbox is still clean, no change necessary.')
+                else:
+                    translatedMsg = wikipedia.translate(mySite, msg)
+                    sandboxPage.put(translatedContent, translatedMsg)
+            except wikipedia.EditConflict:
+                wikipedia.output(u'*** Loading again because of edit conflict.\n')
+            wikipedia.output(u'\nSleeping %s hours, now %s' % (self.hours, now))
+            time.sleep(self.hours * 60 * 60)
+
+def main():
+    for arg in wikipedia.handleArgs():
+        if arg.startswith('-hours:'):
+            hours = float(arg[7:])
+        else:
+            wikipedia.showHelp('clean_sandbox')
+
+        if hours:
+            bot = SandboxBot(hours)
+            bot.run()
+
 if __name__ == "__main__":
     try:
-        for arg in wikipedia.handleArgs():
-            if arg.startswith('-hours:'):
-                hours = float(arg[7:])
-                SandboxBot(hours)
-            else:
-                wikipedia.showHelp('clean_sandbox')
-    except:
+        main()
+    finally:
         wikipedia.stopme()
