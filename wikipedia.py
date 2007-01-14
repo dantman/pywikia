1ef2ab3b404c99d29c376ac43e7d094c8bae243a
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-01-14 15:09:48 +0000

    updated image history regex because mediawiki has changed
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3127 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -1695,7 +1695,8 @@ class ImagePage(Page):
     def getFileVersionHistory(self):
         result = []
         history = re.search('(?s)<ul class="special">.+?</ul>', self.getImagePageContents()).group()
-        lineR = re.compile('<li> \(.+?\) \(.+?\) <a href=".+?" title=".+?">(?P<datetime>.+?)</a> . . <a href=".+?" title=".+?">(?P<username>.+?)</a> \(<a href=".+?" title=".+?">.+?</a>\) . . (?P<resolution>\d+.+?\d+) \((?P<size>[\d,]+) .+?\)( <span class="comment">(?P<comment>.*?)</span>)?</li>')
+        lineR = re.compile('<li> \(.+?\) \(.+?\) <a href=".+?" title=".+?">(?P<datetime>.+?)</a> . . <a href=".+?" title=".+?">(?P<username>.+?)</a> \(.+?\) . . (?P<resolution>\d+.+?\d+) \((?P<size>[\d,\.]+) .+?\)( <span class="comment">(?P<comment>.*?)</span>)?</li>')
+
         for match in lineR.finditer(history):
             datetime = match.group('datetime')
             username = match.group('username')
