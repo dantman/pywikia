e765d80c2afe108871ebd9117cf0a7a323e500c2
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-01-14 13:21:41 +0000

    made it work again by changing the way arguments are passed
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3123 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- delete.py
+++ delete.py
@@ -24,63 +24,41 @@ Delete everything in the category "To delete" without prompting.
 #
 import wikipedia, config, catlib
 import pagegenerators
-#import re, sys, string
+
+# Summary messages for deleting from a category.
+msg_delete_category = {
+    'de': u'Bot: Lösche alle Seiten in Kategorie %s',
+    'en': u'Robot - Deleting all pages from category %s',
+    'lt': u'robotas: Trinami visi puslapiai iš kategorijos %s',
+    'pt': u'Bot: Apagando todas as páginas da categoria %s',
+}
+msg_delete_links = {
+    'de': u'Bot: Lösche alle Seiten in %s verlinkten Seiten',
+    'en': u'Robot - Deleting all pages linked from %s',
+    'lt': u'robotas: Trinami visi puslapiai į kuriuos yra nuoroda iš %s',
+    'pt': u'Bot: Apagando todas as páginas ligadas a %s',
+}
+msg_delete_ref = {
+    'de': u'Bot: Lösche alle auf %s linkenden Seiten',
+    'en': u'Robot - Deleting all pages referring from %s',
+    'lt': u'robotas: Trinami visi puslapiai rodantys į %s',
+    'pt': u'Bot: Apagando todas as páginas afluentes a %s',
+}
 
 class DeletionRobot:
     """
     This robot allows deletion of pages en masse.
     """
-    # Summary messages for deleting from a category.
-    msg_delete_category = {
-        'de': u'Bot: Lösche alle Seiten in Kategorie %s',
-        'en': u'Robot - Deleting all pages from category %s',
-        'lt': u'robotas: Trinami visi puslapiai iš kategorijos %s',
-        'pt': u'Bot: Apagando todas as páginas da categoria %s',
-    }
-    msg_delete_links = {
-        'de': u'Bot: Lösche alle Seiten in %s verlinkten Seiten',
-        'en': u'Robot - Deleting all pages linked from %s',
-        'lt': u'robotas: Trinami visi puslapiai į kuriuos yra nuoroda iš %s',
-        'pt': u'Bot: Apagando todas as páginas ligadas a %s',
-    }
-    msg_delete_ref = {
-        'de': u'Bot: Lösche alle auf %s linkenden Seiten',
-        'en': u'Robot - Deleting all pages referring from %s',
-        'lt': u'robotas: Trinami visi puslapiai rodantys į %s',
-        'pt': u'Bot: Apagando todas as páginas afluentes a %s',
-    }
     
-    def __init__(self, generator, pageName, summary, always = False, doCategory = False, doLinks = False, doRef = False, singlePage = False):
+    def __init__(self, generator, summary, always = False):
         """
         Arguments:
             * generator - A page generator.
-            * pageName - The name of the page the pages to be deleted are collected from.
-            * summary - A custom edit summary.
+            * always - Delete without prompting?
         """
         self.generator = generator
-        self.pageName = pageName
         self.summary = summary
         self.always = always
-        self.doCategory = doCategory
-        self.doLinks = doLinks
-        self.doRef = doRef
-        self.singlePage = singlePage
-
-        # get edit summary message
-        mysite = wikipedia.getSite()
-        if self.summary:
-            wikipedia.setAction(self.summary)
-        else:
-            if self.doCategory:
-                self.summary = wikipedia.translate(mysite, self.msg_delete_category) % self.pageName
-            elif self.doLinks:
-                self.summary = wikipedia.translate(mysite, self.msg_delete_links) % self.pageName
-            elif self.doRef:
-                self.summary = wikipedia.translate(mysite, self.msg_delete_ref) % self.pageName
-            elif self.singlePage:
-                reason = ''
-                self.summary = wikipedia.setAction(reason)
-            wikipedia.setAction(self.summary)
 
     def run(self):
         """
@@ -89,69 +67,83 @@ class DeletionRobot:
         #Loop through everything in the page generator and delete it.
         for page in self.generator:
             wikipedia.output(u'Processing page %s' % page.title())
-            page.delete(self.summary, not self.always, throttle=True)
+            page.delete(self.summary, not self.always, throttle = True)
     
 def main():
     pageName = ''
+    singlePage = ''
     summary = ''
     always = False
-    doCategory = doRef = doLinks = False
+    doSinglePage = False
+    doCategory = False
+    doRef = False
+    doLinks = False
+    gen = None
+    
     # read command line parameters
     for arg in wikipedia.handleArgs():
         if arg == '-always':
             always = True
         elif arg.startswith('-summary'):
             if len(arg) == len('-summary'):
-                summary = wikipedia.input(u'Input an edit summary:')
+                summary = wikipedia.input(u'Enter a reason for the deletion:')
             else:
                 summary = arg[len('-summary:'):]
         elif arg.startswith('-category'):
             doCategory = True
             if len(arg) == len('-category'):
-                pageName = wikipedia.input(u'Input the category to delete from:')
+                pageName = wikipedia.input(u'Enter the category to delete from:')
             else:
                 pageName = arg[len('-category:'):]
         elif arg.startswith('-links'):
             doLinks = True
             if len(arg) == len('-links'):
-                pageName = wikipedia.input(u'Input the page to delete from:')
+                pageName = wikipedia.input(u'Enter the page to delete from:')
             else:
                 pageName = arg[len('-links:'):]
         elif arg.startswith('-ref'):
             doRef = True
             if len(arg) == len('-ref'):
-                pageName = wikipedia.input(u'Input the page to delete from:')
+                pageName = wikipedia.input(u'Enter the page to delete from:')
             else:
                 pageName = arg[len('-ref:'):]
         elif arg.startswith('-page'):
-            singlePage = True
+            doSinglePage = True
             if len(arg) == len('-page'):
-                singlePage = wikipedia.input(u'Input the page to delete:')
+                pageName = wikipedia.input(u'Enter the page to delete:')
             else:
-                singlePage = arg[len('-page:'):]
+                pageName = arg[len('-page:'):]
 
-    if singlePage:
-        page = wikipedia.Page(wikipedia.getSite(), singlePage)
+    mysite = wikipedia.getSite()
+
+    if doSinglePage:
+        if not summary:
+            summary = wikipedia.input(u'Enter a reason for the deletion:')
+        page = wikipedia.Page(wikipedia.getSite(), pageName)
         gen = iter([page])
-        preloadingGen = pagegenerators.PreloadingGenerator(gen)
-        bot = DeletionRobot(preloadingGen, pageName, summary, always, doCategory=doCategory, doLinks=doLinks, doRef=doRef, singlePage=singlePage)
-        bot.run()
-        
     elif doCategory:
+        if not summary:
+            summary = wikipedia.translate(mysite, msg_delete_category) % pageName
         ns = wikipedia.getSite().category_namespace()
         categoryPage = catlib.Category(wikipedia.getSite(), ns + ':' + pageName)
         gen = pagegenerators.CategorizedPageGenerator(categoryPage, recurse=True)
     elif doLinks:
+        if not summary:
+            summary = wikipedia.translate(mysite, msg_delete_links) % pageName
+        wikipedia.setAction(summary)
         linksPage = wikipedia.Page(wikipedia.getSite(), pageName)
         gen = pagegenerators.LinkedPageGenerator(linksPage)
     elif doRef:
+        if not summary:
+            summary = wikipedia.translate(mysite, msg_delete_ref) % pageName
         refPage = wikipedia.Page(wikipedia.getSite(), pageName)
         gen = pagegenerators.ReferringPageGenerator(refPage)
-    if pageName:
+    if gen:
+        wikipedia.setAction(summary)
         preloadingGen = pagegenerators.PreloadingGenerator(gen)
-        bot = DeletionRobot(preloadingGen, pageName, summary, always, doCategory=doCategory, doLinks=doLinks, doRef=doRef, singlePage=singlePage)
+        bot = DeletionRobot(preloadingGen, summary, always)
         bot.run()
-    elif pageName:
+    else:
         wikipedia.showHelp(u'delete')
 
 if __name__ == "__main__":
