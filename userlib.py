f404fa316b9c0f86160d00ac0cb40e96996740eb
misza13 <misza13@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-03 16:01:56 +0000

    Documentation cleanup, reindenting whole file.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3224 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- userlib.py
+++ userlib.py
@@ -7,11 +7,11 @@ import wikipedia
 import re, httplib
 
 class AutoblockUserError(wikipedia.Error):
-  """
-  The class AutoblockUserError is an exception that is raised whenever
-  an action is requested on a virtual autoblock user that's not available
-  for him (i.e. roughly everything except unblock).
-  """
+    """
+    The class AutoblockUserError is an exception that is raised whenever
+    an action is requested on a virtual autoblock user that's not available
+    for him (i.e. roughly everything except unblock).
+    """
 
 class BlockError(wikipedia.Error): pass
 
@@ -24,196 +24,195 @@ class BlockIDError(UnblockError): pass
 class AlreadyUnblockedError(UnblockError): pass
 
 class User:
-  """
-  A class that represents a Wiki user.
-  Has getters for the user's User: an User talk: (sub-)pages,
-  as well as methods for blocking and unblocking (in future).
-  """
-
-  #TODO list:
-  #- block and unblock methods
-  #- browse contributions, maybe?
-
-  def __init__(self, site, name):
     """
-    Initializer for a User object.
-
-    Parameters:
-    site - a wikipedia.Site object
-    name - name of the user, without the trailing User:
+    A class that represents a Wiki user.
+    Has getters for the user's User: an User talk: (sub-)pages,
+    as well as methods for blocking and unblocking.
     """
 
-    self.site = site
-    self.name = name
+    #TODO list:
+    #- browse contributions
 
-  def getUserPage(self, subpage=''):
-    if self.name[0] == '#':
-      #This user is probably being queried for purpose of lifting
-      #an autoblock, so has no user pages per se.
-      raise AutoblockUserError
-    fullpagename = self.site.namespace(2) + ':' + self.name
-    if subpage:
-      fullpagename += '/' + subpage
-    return wikipedia.Page(self.site,fullpagename)
+    def __init__(self, site, name):
+        """
+        Initializer for a User object.
 
-  def getUserTalkPage(self, subpage=''):
-    if self.name[0] == '#':
-      #This user is probably being queried for purpose of lifting
-      #an autoblock, so has no user talk pages per se.
-      raise AutoblockUserError
-    fullpagename = self.site.namespace(3) + ':' + self.name
-    if subpage:
-      fullpagename += '/' + subpage
-    return wikipedia.Page(self.site,fullpagename)
+        Parameters:
+        site - a wikipedia.Site object
+        name - name of the user, without the trailing User:
+        """
 
-  def block(self, expiry=None, reason=None, AnonOnly=True, NoSignup=False, EnableAutoblock=False):
-    """
-    Block the user.
+        self.site = site
+        self.name = name
 
-    Parameters:
-    expiry - expiry time of block, may be a period of time (incl. infinite)
-             or the block's expiry time
-    reason - reason for block
-    AnonOnly - is the block affecting only anonymous users?
-    NoSignup - does the block disable account creation?
-    EnableAutoblock - is autoblock enabled on the block?
+    def getUserPage(self, subpage=''):
+        if self.name[0] == '#':
+            #This user is probably being queried for purpose of lifting
+            #an autoblock, so has no user pages per se.
+            raise AutoblockUserError
+        fullpagename = self.site.namespace(2) + ':' + self.name
+        if subpage:
+            fullpagename += '/' + subpage
+        return wikipedia.Page(self.site,fullpagename)
 
-    The default values for block options are set to as most unrestrictive
-    """
+    def getUserTalkPage(self, subpage=''):
+        if self.name[0] == '#':
+            #This user is probably being queried for purpose of lifting
+            #an autoblock, so has no user talk pages per se.
+            raise AutoblockUserError
+        fullpagename = self.site.namespace(3) + ':' + self.name
+        if subpage:
+            fullpagename += '/' + subpage
+        return wikipedia.Page(self.site,fullpagename)
 
-    if self.name[0] == '#':
-      #This user is probably being queried for purpose of lifting
-      #an autoblock, so can't be blocked.
-      raise AutoblockUserError
+    def block(self, expiry=None, reason=None, AnonOnly=True, NoSignup=False, EnableAutoblock=False):
+        """
+        Block the user.
 
-    if expiry == None:
-      expiry = input(u'Please enter the expiry time for the block:')
-    if reason == None:
-      reason = input(u'Please enter a reason for the block:')
+        Parameters:
+        expiry - expiry time of block, may be a period of time (incl. infinite)
+                 or the block's expiry time
+        reason - reason for block
+        AnonOnly - is the block affecting only anonymous users?
+        NoSignup - does the block disable account creation?
+        EnableAutoblock - is autoblock enabled on the block?
 
-    token = self.site.getToken(self, sysop = True)
+        The default values for block options are set to as most unrestrictive
+        """
 
-    wikipedia.output(u"Blocking [[User:%s]]..." % self.name)
+        if self.name[0] == '#':
+            #This user is probably being queried for purpose of lifting
+            #an autoblock, so can't be blocked.
+            raise AutoblockUserError
 
-    boolStr = ['0','1']
-    predata = [
-        ('wpBlockAddress', self.name),
-        ('wpBlockExpiry', 'other'),
-        ('wpBlockOther', expiry),
-        ('wpBlockReason', reason),
-        ('wpAnonOnly', boolStr[AnonOnly]),
-        ('wpCreateAccount', boolStr[NoSignup]),
-        ('wpEnableAutoblock', boolStr[EnableAutoblock]),
-        ('wpBlock', 'Block this user'),
-        ('wpEditToken', token)
-        ]
+        if expiry == None:
+            expiry = input(u'Please enter the expiry time for the block:')
+        if reason == None:
+            reason = input(u'Please enter a reason for the block:')
 
-    data = wikipedia.urlencode(tuple(predata))
-    address = self.site.block_address()
+        token = self.site.getToken(self, sysop = True)
 
-    conn = httplib.HTTPConnection(self.site.hostname())
-    conn.putrequest("POST", address)
-    conn.putheader('Content-Length', str(len(data)))
-    conn.putheader("Content-type", "application/x-www-form-urlencoded")
-    conn.putheader("User-agent", wikipedia.useragent)
-    conn.putheader('Cookie', self.site.cookies())
-    conn.endheaders()
-    conn.send(data)
+        wikipedia.output(u"Blocking [[User:%s]]..." % self.name)
 
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
+        boolStr = ['0','1']
+        predata = [
+            ('wpBlockAddress', self.name),
+            ('wpBlockExpiry', 'other'),
+            ('wpBlockOther', expiry),
+            ('wpBlockReason', reason),
+            ('wpAnonOnly', boolStr[AnonOnly]),
+            ('wpCreateAccount', boolStr[NoSignup]),
+            ('wpEnableAutoblock', boolStr[EnableAutoblock]),
+            ('wpBlock', 'Block this user'),
+            ('wpEditToken', token)
+            ]
 
-    if response.status != 302:
-      if re.search('is already blocked',data):
-        raise AlreadyBlockedError
-      raise BlockError
-    return True
+        data = wikipedia.urlencode(tuple(predata))
+        address = self.site.block_address()
 
-  def unblock(self, reason=None):
-    """
-    Unblock the user.
+        conn = httplib.HTTPConnection(self.site.hostname())
+        conn.putrequest("POST", address)
+        conn.putheader('Content-Length', str(len(data)))
+        conn.putheader("Content-type", "application/x-www-form-urlencoded")
+        conn.putheader("User-agent", wikipedia.useragent)
+        conn.putheader('Cookie', self.site.cookies())
+        conn.endheaders()
+        conn.send(data)
 
-    Parameter:
-    reason - reason for block
-    """
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
 
-    if self.name[0] == '#':
-      blockID = self.name[1:]
-    else:
-      blockID = self._getBlockID()
+        if response.status != 302:
+            if re.search('is already blocked',data):
+                raise AlreadyBlockedError
+            raise BlockError
+        return True
 
-    self._unblock(blockID,reason)
+    def unblock(self, reason=None):
+        """
+        Unblock the user.
 
-  def _getBlockID(self):
-    wikipedia.output(u"Getting block id for [[User:%s]]..." % self.name)
+        Parameter:
+        reason - reason for block
+        """
 
-    token = self.site.getToken(self, sysop = True)
-    address = self.site.blocksearch_address(self.name)
+        if self.name[0] == '#':
+            blockID = self.name[1:]
+        else:
+            blockID = self._getBlockID()
 
-    conn = httplib.HTTPConnection(self.site.hostname())
-    conn.putrequest("GET", address)
-    conn.putheader("User-agent", wikipedia.useragent)
-    conn.putheader('Cookie', self.site.cookies())
-    conn.endheaders()
-    conn.send('')
+        self._unblock(blockID,reason)
 
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
+    def _getBlockID(self):
+        wikipedia.output(u"Getting block id for [[User:%s]]..." % self.name)
 
-    bIDre = re.search(r'action=unblock&amp;id=(\d+)',data)
-    if not bIDre:
-      print data
-      raise BlockIDError
+        token = self.site.getToken(self, sysop = True)
+        address = self.site.blocksearch_address(self.name)
 
-    return bIDre.group(1)
+        conn = httplib.HTTPConnection(self.site.hostname())
+        conn.putrequest("GET", address)
+        conn.putheader("User-agent", wikipedia.useragent)
+        conn.putheader('Cookie', self.site.cookies())
+        conn.endheaders()
+        conn.send('')
 
-  def _unblock(self, blockID, reason):
-    wikipedia.output(u"Unblocking [[User:%s]]..." % self.name)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
 
-    token = self.site.getToken(self, sysop = True)
+        bIDre = re.search(r'action=unblock&amp;id=(\d+)',data)
+        if not bIDre:
+            print data
+            raise BlockIDError
 
-    predata = [
-        ('id', blockID),
-        ('wpUnblockReason', reason),
-        ('wpBlock', 'Unblock this address'),
-        ('wpEditToken', token)
-        ]
+        return bIDre.group(1)
 
-    data = wikipedia.urlencode(tuple(predata))
-    address = self.site.unblock_address()
+    def _unblock(self, blockID, reason):
+        wikipedia.output(u"Unblocking [[User:%s]]..." % self.name)
 
-    conn = httplib.HTTPConnection(self.site.hostname())
-    conn.putrequest("POST", address)
-    conn.putheader('Content-Length', str(len(data)))
-    conn.putheader("Content-type", "application/x-www-form-urlencoded")
-    conn.putheader("User-agent", wikipedia.useragent)
-    conn.putheader('Cookie', self.site.cookies())
-    conn.endheaders()
-    conn.send(data)
+        token = self.site.getToken(self, sysop = True)
 
-    response = conn.getresponse()
-    data = response.read()
-    conn.close()
+        predata = [
+            ('id', blockID),
+            ('wpUnblockReason', reason),
+            ('wpBlock', 'Unblock this address'),
+            ('wpEditToken', token),
+            ]
 
-    if response.status != 302:
-      print data
-      if re.search('Block ID \d+ not found',data):
-        raise AlreadyUnblockedError
-      raise UnblockError
-    return True
+        data = wikipedia.urlencode(tuple(predata))
+        address = self.site.unblock_address()
+
+        conn = httplib.HTTPConnection(self.site.hostname())
+        conn.putrequest("POST", address)
+        conn.putheader('Content-Length', str(len(data)))
+        conn.putheader("Content-type", "application/x-www-form-urlencoded")
+        conn.putheader("User-agent", wikipedia.useragent)
+        conn.putheader('Cookie', self.site.cookies())
+        conn.endheaders()
+        conn.send(data)
+
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+
+        if response.status != 302:
+            print data
+            if re.search('Block ID \d+ not found',data):
+                raise AlreadyUnblockedError
+            raise UnblockError
+        return True
 
 
 if __name__ == '__main__':
-  """
-  Simple testing code for the [[User:Example]] on the English Wikipedia.
-  """
-  try:
-    Site = wikipedia.getSite()
-    exampleUser = User(Site,'Example')
-    print exampleUser.getUserPage().get()
-    print exampleUser.getUserPage('Lipsum').get()
-    print exampleUser.getUserTalkPage().get()
-  finally:
-    wikipedia.stopme()
+    """
+    Simple testing code for the [[User:Example]] on the English Wikipedia.
+    """
+    try:
+        Site = wikipedia.getSite()
+        exampleUser = User(Site,'Example')
+        print exampleUser.getUserPage().get()
+        print exampleUser.getUserPage('Lipsum').get()
+        print exampleUser.getUserTalkPage().get()
+    finally:
+        wikipedia.stopme()
