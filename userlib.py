0f9e95164d2aa979ea5f77b378611819ccca798f
misza13 <misza13@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-03 14:19:07 +0000

    Added a working block() method (might now work on all mediawikis yet).
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3220 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- userlib.py
+++ userlib.py
@@ -4,6 +4,7 @@ Library to work with users, their pages and talk pages.
 """
 
 import wikipedia
+import httplib
 
 class AutoblockUserError(wikipedia.Error):
   """
@@ -35,7 +36,7 @@ class User:
     self.site = site
     self.name = name
 
-  def getUserPage(self,subpage=''):
+  def getUserPage(self, subpage=''):
     if self.name[0] == '#':
       #This user is probably being queried for purpose of lifting
       #an autoblock, so has no user pages per se.
@@ -45,7 +46,7 @@ class User:
       fullpagename += '/' + subpage
     return wikipedia.Page(self.site,fullpagename)
 
-  def getUserTalkPage(self,subpage=''):
+  def getUserTalkPage(self, subpage=''):
     if self.name[0] == '#':
       #This user is probably being queried for purpose of lifting
       #an autoblock, so has no user talk pages per se.
@@ -54,6 +55,60 @@ class User:
     if subpage:
       fullpagename += '/' + subpage
     return wikipedia.Page(self.site,fullpagename)
+
+  def block(self, expiry=None, reason=None, AnonOnly=True, NoSignup=False, EnableAutoblock=False):
+    """
+    Block the user.
+
+    Parameters:
+    expiry - expiry time of block, may be a period of time (incl. infinite)
+             or the block's expiry time
+    reason - reason for block
+    AnonOnly - is the block affecting only anonymous users?
+    NoSignup - does the block disable account creation?
+    EnableAutoblock - is autoblock enabled on the block?
+
+    The default values for block options are set to as most unrestrictive
+    """
+
+    if expiry == None:
+      expiry = input(u'Please enter the expiry time for the block:')
+    if reason == None:
+      reason = input(u'Please enter a reason for the block:')
+
+    token = self.site.getToken(self, sysop = True)
+
+    wikipedia.output(u"Blocking [[User:%s]]..." % self.name)
+
+    boolStr = ['0','1']
+
+    predata = [
+        ('wpBlockAddress', self.name),
+        ('wpBlockExpiry', 'other'),
+        ('wpBlockOther', expiry),
+        ('wpBlockReason', reason),
+        ('wpAnonOnly', boolStr[AnonOnly]),
+        ('wpCreateAccount', boolStr[NoSignup]),
+        ('wpEnableAutoblock', boolStr[EnableAutoblock]),
+        ('wpBlock', 'Block this user'),
+        ('wpEditToken', token)
+        ]
+
+    data = wikipedia.urlencode(tuple(predata))
+    conn = httplib.HTTPConnection(self.site.hostname())
+    conn.putrequest("POST", '/w/index.php?title=Special:Blockip&action=submit')
+    conn.putheader('Content-Length', str(len(data)))
+    conn.putheader("Content-type", "application/x-www-form-urlencoded")
+    conn.putheader("User-agent", wikipedia.useragent)
+    conn.putheader('Cookie', self.site.cookies())
+    conn.endheaders()
+    conn.send(data)
+
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+
+    return response
 
 if __name__ == '__main__':
   """
