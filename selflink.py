32e8a4e884aae2eb942bf170405335559e27a816
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-09 01:56:11 +0000

    more elegant solution by using recursion
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3247 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- selflink.py
+++ selflink.py
@@ -84,67 +84,66 @@ class SelflinkBot:
         # note that the definition of 'letter' varies from language to language.
         self.linkR = re.compile(r'\[\[(?P<title>[^\]\|#]*)(?P<section>#[^\]\|]*)?(\|(?P<label>[^\]]*))?\]\](?P<linktrail>' + linktrail + ')')
 
-    def treat(self, page):
-            # how many bytes should be displayed around the current link
-            context = 90
-            moreContext = 400
-            # Show the title of the page where the link was found.
-            # Highlight the title in purple.
-            colors = [None] * 6 + [13] * len(page.title()) + [None] * 4
-            wikipedia.output(u"\n\n>>> %s <<<" % page.title(), colors = colors)
-            try:
-                oldText = page.get()
-                text = oldText
-                curpos = 0
-                while curpos < len(text):
-                    m = self.linkR.search(text, pos = curpos)
-                    if not m:
-                        break
-                    # Make sure that next time around we will not find this same hit.
-                    curpos = m.start() + 1
-                    # ignore interwiki links and links to sections of the same page as well as section links
-                    if not m.group('title') or page.site().isInterwikiLink(m.group('title')) or m.group('section'):
-                        continue
-                    else:
-                        linkedPage = wikipedia.Page(page.site(), m.group('title'))
-                    # Check whether the link found is to the current page itself.
-                    if linkedPage == page:
-                        # at the beginning of the link, start red color.
-                        # at the end of the link, reset the color to default
-                        colors = [None for c in text[max(0, m.start() - context) : m.start()]] + [12 for c in text[m.start() : m.end()]] + [None for c in text[m.end() : m.end() + context]]
-                        wikipedia.output(text[max(0, m.start() - context) : m.end() + context], colors = colors)
-                        choice = wikipedia.inputChoice(u'What shall be done with this selflink?',  ['unlink', 'make bold', 'skip', 'more context'], ['U', 'b', 's', 'm'], 'u')
-                        print
-                        if choice != 's':
-                            new = m.group('label') or m.group('title')
-                            new += m.group('linktrail')
-                            if choice == 'u':
-                                text = text[:m.start()] + new + text[m.end():]
-                            elif choice == 'm':
-                                colors = [None for c in text[max(0, m.start() - moreContext) : m.start()]] + [12 for c in text[m.start() : m.end()]] + [None for c in text[m.end() : m.end() + moreContext]]
-                                wikipedia.output(text[max(0, m.start() - moreContext) : m.end() + moreContext], colors = colors)
-                                choice = wikipedia.inputChoice(u'What shall be done with this selflink?',  ['unlink', 'make bold', 'skip'], ['U', 'b', 's'], 'u')
-                                print
-                                if choice != 's':
-                                    new = m.group('label') or m.group('title')
-                                    new += m.group('linktrail')
-                                    if choice == 'u':
-                                        text = text[:m.start()] + new + text[m.end():]
-                                    else:
-                                        text = text[:m.start()] + "'''" + new + "'''" + text[m.end():]
-                            else:
-                                text = text[:m.start()] + "'''" + new + "'''" + text[m.end():]
-                if oldText == text:
-                    wikipedia.output(u'No changes necessary.')
+    def handleNextLink(self, page, text, match, context = 100):
+        # ignore interwiki links and links to sections of the same page as well as section links
+        if not match.group('title') or page.site().isInterwikiLink(match.group('title')) or match.group('section'):
+            return text
+
+        linkedPage = wikipedia.Page(page.site(), match.group('title'))
+        # Check whether the link found is to the current page itself.
+        if linkedPage != page:
+            # not a self-link
+            return text
+        else:
+            # at the beginning of the link, start red color.
+            # at the end of the link, reset the color to default
+            colors = [None for c in text[max(0, match.start() - context) : match.start()]] + [12 for c in text[match.start() : match.end()]] + [None for c in text[match.end() : match.end() + context]]
+            wikipedia.output(text[max(0, match.start() - context) : match.end() + context], colors = colors)
+            choice = wikipedia.inputChoice(u'\nWhat shall be done with this selflink?',  ['unlink', 'make bold', 'skip', 'more context'], ['U', 'b', 's', 'm'], 'u')
+            wikipedia.output(u'')
+
+            if choice == 's':
+                # skip this link
+                return text
+            else:
+                new = match.group('label') or match.group('title')
+                new += match.group('linktrail')
+                if choice == 'u':
+                    return text[:match.start()] + new + text[match.end():]
+                elif choice == 'b':
+                    return text[:match.start()] + "'''" + new + "'''" + text[match.end():]
                 else:
-                    wikipedia.showDiff(oldText, text)
-                    page.put(text)
-            except wikipedia.NoPage:
-                wikipedia.output(u"Page %s does not exist?!" % page.aslink())
-            except wikipedia.IsRedirectPage:
-                wikipedia.output(u"Page %s is a redirect; skipping." % page.aslink())
-            except wikipedia.LockedPage:
-                wikipedia.output(u"Page %s is locked?!" % page.aslink())
+                    # show more context by recursive self-call
+                    return self.handleNextLink(page, text, match, context = context + 100)
+    
+    def treat(self, page):
+        # Show the title of the page where the link was found.
+        # Highlight the title in purple.
+        colors = [None] * 6 + [13] * len(page.title()) + [None] * 4
+        wikipedia.output(u"\n\n>>> %s <<<" % page.title(), colors = colors)
+        try:
+            oldText = page.get()
+            text = oldText
+            curpos = 0
+            while curpos < len(text):
+                match = self.linkR.search(text, pos = curpos)
+                if not match:
+                    break
+                # Make sure that next time around we will not find this same hit.
+                curpos = match.start() + 1
+                text = self.handleNextLink(page, text, match)
+
+            if oldText == text:
+                wikipedia.output(u'No changes necessary.')
+            else:
+                wikipedia.showDiff(oldText, text)
+                page.put(text)
+        except wikipedia.NoPage:
+            wikipedia.output(u"Page %s does not exist?!" % page.aslink())
+        except wikipedia.IsRedirectPage:
+            wikipedia.output(u"Page %s is a redirect; skipping." % page.aslink())
+        except wikipedia.LockedPage:
+            wikipedia.output(u"Page %s is locked?!" % page.aslink())
 
     def run(self):
         comment = wikipedia.translate(wikipedia.getSite(), msg)
