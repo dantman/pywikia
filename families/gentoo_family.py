a7b5af73b99d6fba0cb0ae4800ccbc29cde6a81d
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-15 23:39:31 +0000

    added comment
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3298 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/gentoo_family.py
+++ families/gentoo_family.py
@@ -7,6 +7,8 @@ __version__ = '$Id$'
 
 # An inofficial Gentoo wiki project.
 # Ask for permission at http://gentoo-wiki.com/Help:Bots before running a bot.
+# Be very careful, and set a long throttle: "until we see it is good one edit
+# ever minute and one page fetch every 30 seconds, maybe a *bit* faster later".
 
 class Family(family.Family):
 
