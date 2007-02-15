f03bcfd89617618bed5c402203662f79fcdda45a
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-15 23:30:31 +0000

    namespaces, langs by size
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3295 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/gentoo_family.py
+++ families/gentoo_family.py
@@ -8,23 +8,29 @@ __version__ = '$Id$'
 # An inofficial Gentoo wiki project
 
 class Family(family.Family):
-    
+
     def __init__(self):
         family.Family.__init__(self)
-        self.name = 'gentoowiki'
+        self.name = 'gentoo'
 
         self.langs = {
             'en':'gentoo-wiki.com',
-            'nl':'nl.gentoo-wiki.com',
-            'hu':'hu.gentoo-wiki.com',
-            'ru':'ru.gentoo-wiki.com',
-            'zh':'zh.gentoo-wiki.com',
-            'he':'he.gentoo-wiki.com',
-            'fr':'fr.gentoo-wiki.com',
             'de':'de.gentoo-wiki.com',
             'es':'es.gentoo-wiki.com',
+            'fr':'fr.gentoo-wiki.com',
+            'he':'he.gentoo-wiki.com',
+            'hu':'hu.gentoo-wiki.com',
+            'nl':'nl.gentoo-wiki.com',
             'pt':'pt.gentoo-wiki.com',
+            'ru':'ru.gentoo-wiki.com',
+            'zh':'zh.gentoo-wiki.com',
         }
+
+        # TODO: sort
+        self.languages_by_size = ['en', 'de', 'es', 'fr', 'he', 'hu', 'nl', 'pt', 'ru', 'zh']
+
+        # he: also uses the default 'Media'
+        del self.namespaces[-2]['he']
 
         self.namespaces[4] = {
             '_default': [u'Gentoo Linux Wiki', self.namespaces[4]['_default']],
@@ -32,6 +38,13 @@ class Family(family.Family):
         self.namespaces[5] = {
             '_default': [u'Gentoo Linux Wiki talk', self.namespaces[5]['_default']],
             'de': u'Gentoo Linux Wiki Diskussion',
+            'es': u'Gentoo Linux Wiki Discusión',
+            'fr': u'Discussion Gentoo Linux Wiki',
+            'he': u'שיחת Gentoo Linux Wiki',
+            'hu': u'Gentoo Linux Wiki vita',
+            'nl': u'Overleg Gentoo Linux Wiki',
+            'pt': u'Gentoo Linux Wiki Discussão',
+            'ru': u'Обсуждение Gentoo Linux Wiki',
         }
         self.namespaces[100] = {
             '_default': [u'Index'],
