03ac8043b923c827f50d7a67f0f234fc73440da8
misza13 <misza13@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-15 18:35:27 +0000

    Updating after wiki has moved.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3292 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/supertux_family.py
+++ families/supertux_family.py
@@ -12,7 +12,7 @@ class Family(family.Family):
         family.Family.__init__(self)
         self.name = 'supertux'
         self.langs = {
-            'en': 'supertux.berlios.de',
+            'en': 'supertux.lethargik.org',
         }
         
         self.namespaces[4] = {
@@ -25,10 +25,5 @@ class Family(family.Family):
     def path(self, code):
         return '/wiki/index.php'
 
-    def querypath(self, code):
-        return '/wiki/query.php'
-
-
     def version(self, code):
-        return "1.5"
-
+        return "1.9.2"
