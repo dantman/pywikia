f8410299ee71071150461e3b9fefdc43f3f63b2e
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-21 22:16:55 +0000

    namespaces
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3314 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/freeciv_family.py
+++ families/freeciv_family.py
@@ -19,15 +19,28 @@ class Family(family.Family):
             'fi': 'fi.freeciv.wikia.com',
             'fr': 'fr.freeciv.wikia.com',
         }
-        
+
         self.namespaces[4] = {
-            '_default': [u'Freeciv', self.namespaces[4]['_default']],
+            '_default': [
+                u'Freeciv',
+                self.namespaces[4]['_default']
+            ],
+            'fi': u'FreeCiv wiki Suomalaisille',
         }
+
         self.namespaces[5] = {
-            '_default': [u'Freeciv talk', self.namespaces[5]['_default']],
-            'fr': u'Discussion Freeciv',
-            'de': u'Freeciv Diskussion',
+            '_default': [
+                lambda code: self._talkNamespace(code, 4),
+                lambda code: self._talkNamespace('_default', 4),
+                self.namespaces[5]['_default']
+            ],
+            'fi': u'Keskustelu FreeCiv wiki Suomalaisillesta',
         }
+
+        self.namespaces[8]['fi'] = u'MediaWiki'
+
+        self.namespaces[9]['da'] = u'MediaWiki diskussion'
+        self.namespaces[9]['fi'] = u'MediaWiki talk'
 
         self.namespaces[110] = {
             '_default': u'Forum',
@@ -36,6 +49,8 @@ class Family(family.Family):
         self.namespaces[111] = {
             '_default': u'Forum talk',
         }
+
+        self.talkNamespacePattern['da'] = lambda ns: u'%s diskussion' % ns
 
 
     def path(self, code):
