ee4c94a70fb2d53a22e9fa168d31638d097be925
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-28 05:00:11 +0000

    see feature request [ 1501381 ] A script to get pages and run arbitrary shell commands
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3327 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ get.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Very simple script which gets a page and writes its contents to
+standard output. This makes it possible to pipe the text to another
+process.
+
+Syntax: python get.py Title of the page
+
+Example: python get.py Wikipedia | grep MediaWiki > results.txt
+"""
+
+# (C) Daniel Herding, 2005
+#
+# Distributed under the terms of the MIT license.
+
+__version__='$Id$'
+
+import wikipedia
+
+def main():
+    singlePageTitleParts = []
+    for arg in wikipedia.handleArgs():
+        singlePageTitleParts.append(arg)
+
+    pageTitle = " ".join(singlePageTitleParts)
+    page = wikipedia.Page(wikipedia.getSite(), pageTitle)
+
+    # TODO: catch exceptions
+    wikipedia.output(page.get(), toStdout = True)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        wikipedia.stopme()
+        raise
+    else:
+        wikipedia.stopme()
+
