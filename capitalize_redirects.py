12f5403c327b5da174a3199d9cac1929e4e3ac52
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-02-28 12:17:08 +0000

    this script create a redirect of capitalize articles
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3332 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ capitalize_redirects.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8  -*-
+"""
+The following scripts was copied from, with adaptations:
+http://en.wikipedia.org/wiki/User:Drinibot/CapitalizationRedirects
+Copyright es:user:Yrithinnd
+
+TODO:
+* create a cache file to store verified articles. (pickle)
+* translate "redirect" name by language. (import family; fam = Family(); fam.redirect)
+
+"""
+
+import time, sys
+import wikipedia, pagegenerators, catlib
+
+msg = {
+    'en': u'Robot: Create redirects',
+    'pt': u'Bot: Criando redirects',
+    }
+
+class CapitalizeBot:
+    def __init__(self, generator, acceptall = False):
+        self.generator = generator
+        self.acceptall = False
+
+    def run(self):
+        for page in self.generator:
+            page_t = page.title()
+            np = wikipedia.Page(wikipedia.getSite(), page_t.capitalize())
+            if not np.exists():
+                wikipedia.output(u'%s don\'t exist' % np.title())
+                if not self.acceptall:
+                    choice = wikipedia.inputChoice(u'Do you want create redirect?',  ['Yes', 'No', 'All'], ['y', 'N', 'a'], 'N')
+                    if choice in ['a', 'A']:
+                        self.acceptall = True
+                if self.acceptall or choice in ['y', 'Y']:
+                    try:
+                        comment = wikipedia.translate(wikipedia.getSite(), msg)
+                        np.put((u"#REDIRECT [[%s]]" % page_t), comment)
+                        print
+                    except:
+                        wikipedia.output(u"An error occured. Retrying in 15 seconds...")
+                        time.sleep(15)
+                        continue
+            else:
+                wikipedia.output(u'%s already exist, skipping...\n' % np.title())
+    
+def main():
+    gen = None
+    source = None
+    textfilename = None
+    categoryname = None
+    pageNames = []
+    referredPageName = None
+    acceptall = False
+    namespaces = []
+    startpage = None
+
+    for arg in sys.argv[1:]:
+        arg = wikipedia.argHandler(arg, 'capitalize_redirects')
+        if arg:
+            if arg.startswith('-file'):
+                if len(arg) == 5:
+                    textfilename = wikipedia.input(u'Please enter the filename:')
+                else:
+                    textfilename = arg[6:]
+                source = 'textfile'
+            elif arg.startswith('-cat'):
+                if len(arg) == 4:
+                    categoryname = wikipedia.input(u'Please enter the category name:')
+                else:
+                    categoryname = arg[5:]
+                source = 'category'
+            elif arg.startswith('-page'):
+                if len(arg) == 5:
+                    pageNames.append(wikipedia.input(u'Which page do you want to chage?'))
+                else:
+                    pageNames.append(arg[6:])
+                source = 'singlepage'
+            elif arg.startswith('-ref'):
+                if len(arg) == 4:
+                    referredPageName = wikipedia.input(u'Links to which page should be processed?')
+                else:
+                    referredPageName = arg[5:]
+                source = 'ref'
+            elif arg.startswith('-start'):
+                if len(arg) == 6:
+                    firstPageTitle = wikipedia.input(u'Which page do you want to chage?')
+                else:
+                    firstPageTitle = arg[7:]
+                source = 'allpages'
+            elif arg == '-always':
+                acceptall = True
+            elif arg.startswith('-namespace:'):
+                namespaces.append(int(arg[11:]))
+            else:
+                commandline_replacements.append(arg)
+        
+    if source == 'textfile':
+        gen = pagegenerators.TextfilePageGenerator(textfilename)
+    elif source == 'category':
+        cat = catlib.Category(wikipedia.getSite(), categoryname)
+        gen = pagegenerators.CategorizedPageGenerator(cat)
+    elif source == 'singlepage':
+        pages = [wikipedia.Page(wikipedia.getSite(), pageName) for pageName in pageNames]
+        gen = iter(pages)
+    elif source == 'allpages':
+        namespace = wikipedia.Page(wikipedia.getSite(), firstPageTitle).namespace()
+        gen = pagegenerators.AllpagesPageGenerator(firstPageTitle, namespace)
+    elif source == 'ref':
+        referredPage = wikipedia.Page(wikipedia.getSite(), referredPageName)
+        gen = pagegenerators.ReferringPageGenerator(referredPage)
+    elif source == None or len(commandline_replacements) not in [0, 2]:
+        wikipedia.stopme()
+        wikipedia.showHelp(u'capitalize_redirects')
+        sys.exit()
+    if namespaces != []:
+        gen =  pagegenerators.NamespaceFilterPageGenerator(gen, namespaces)
+    preloadingGen = pagegenerators.PreloadingGenerator(gen, pageNumber = 20)
+    bot = CapitalizeBot(preloadingGen, acceptall)
+    bot.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        wikipedia.stopme()
