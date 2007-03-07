3d3d78ed134206ed1ef1346b3479519f9a2febdb
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-03-07 17:11:13 +0000

    by Waldir
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3398 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ unusedfiles.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+This bot appends some text to all unused images and other text to the respective uploaders.
+
+You are asked for confirmation before every change.
+"""
+import wikipedia
+import pagegenerators
+import sys
+
+def appendtext(page, apptext):
+    try:
+        text = page.get()
+    except wikipedia.IsRedirectPage:
+        return
+    # Here go edit text in whatever way you want. If you find you do not  (he meant "Here you can go editing...")
+    # want to edit this page, just return
+    text += apptext;
+    if text != page.get():
+        wikipedia.showDiff(page.get(),text)
+        if wikipedia.input('Do you want to save the changes? (Y/N)') in 'yY':
+            page.put(text,u'marcação de imagens para eliminação')
+
+def main():
+    for arg in wikipedia.handleArgs():
+        start.append(arg)
+
+    mysite = wikipedia.getSite()
+    # If anything needs to be prepared, you can do it here
+
+    basicgenerator = pagegenerators.UnusedFilesGenerator()
+    generator = pagegenerators.PreloadingGenerator(basicgenerator)
+    for page in generator:
+        #print page.title()
+        if '<table id="mw_metadata" class="mw_metadata">' not in page.getImagePageHtml() and 'http://' not in page.get():
+            print '\n' + page.title()
+            appendtext(page,u'\n\n{{subst:No-use2}}')
+            uploader = page.getFileVersionHistory().pop()[1]
+            usertalkname = u'User Talk:' + uploader
+            usertalkpage = wikipedia.Page(mysite,usertalkname)
+            msg2uploader = u'\n\n{{img-sem-uso|' + page.title() + u'}}'
+            appendtext(usertalkpage,msg2uploader)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        wikipedia.stopme()
