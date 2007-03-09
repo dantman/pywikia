d1d2ffd37f6a098be890189d8f4dfb87eccbb950
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-03-09 23:15:19 +0000

    made it work again
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3412 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/mozilla_family.py
+++ families/mozilla_family.py
@@ -7,19 +7,21 @@ import family
 class Family(family.Family):
     def __init__(self):
         family.Family.__init__(self)
-        name = 'mozilla'
-	self.langs = {
-            'en': 'wiki.mozilla.org',
-	}
-	self.namespaces[4] = {
-	    '_default': [u'MozillaWiki', self.namespaces[4]['_default']],
-	    }
-	self.namespaces[5] = {
-	    '_default': [u'MozillaWiki talk', self.namespaces[5]['_default']],
-	    }
-	
+
+        self.name = 'mozilla'
+
+        self.langs = {
+                'en': 'wiki.mozilla.org',
+        }
+        self.namespaces[4] = {
+            '_default': [u'MozillaWiki', self.namespaces[4]['_default']],
+        }
+        self.namespaces[5] = {
+            '_default': [u'MozillaWiki talk', self.namespaces[5]['_default']],
+        }
+
     def version(self, code):
         return "1.4.2"
-    
+
     def path(self, code):
         return '/index.php'
