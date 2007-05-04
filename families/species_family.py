a067455abf2f59bcc7cead7c0648f0760423b688
siebrand <siebrand@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-05-04 00:54:00 +0000

    * adding support for species.wikimedia.org (species:species) in species_family.py (based on meta_family.py) * updating generic namespace names for ay, be-x-old, hy, new, zea (family.py) * updating wikipedia family namespace names for ay, new, sa (wikipedia_family.py)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3508 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

---
+++ families/species_family.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8  -*-
+
+__version__ = '$Id$'
+
+import family
+
+# The species family
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'species'
+        self.langs = {
+            'species': 'species.wikimedia.org',
+           }
+        
+        self.namespaces[4] = {
+            '_default': [u'Wikispecies', self.namespaces[4]['_default']],
+        }
+        self.namespaces[5] = {
+            '_default': [u'Wikispecies talk', self.namespaces[5]['_default']],
+        }
+
+        self.interwiki_forward = 'wikipedia'
