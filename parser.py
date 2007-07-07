7da76335f1be8689552e7dc2732baccfc2d553f3
valhallasw <valhallasw@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-07-07 16:54:25 +0000

    lexer update
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3856 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- parser.py
+++ parser.py
@@ -15,16 +15,13 @@ import xml.dom.minidom as dom
 # System loosely based on 'the dragon book':
 # Compilers, Principles, Techniques and Tools, Aho, Sethi, Ullman, 1st edition, 1986
 
-class OutOfCharsException(Exception):
-    """ lexer pwn error """
-
 class lexer:
     class TEXT:
         """ Text node. Text="""
-    class WL_OPEN:
-        """ Wikilink opener [[ """
-    class WL_CLOSE:
-        """ Wikilink closer ]] """
+    class SQRE_OPEN:
+        """ Square open symbols [ N="""
+    class SQRE_CLOSE:
+        """ Square close symbols ] N="""
     class PIPE:
         """ Pipe symbol """
     class CURL_OPEN:
@@ -32,9 +29,9 @@ class lexer:
     class CURL_CLOSE:
         """ Curl close symbols } N="""
     class ANGL_OPEN:
-        """ Angular open symbol < """
+        """ Angular open symbols < N="""
     class ANGL_CLOSE:
-        """ Anglular close symbol > """
+        """ Anglular close symbol > N="""
     class NEWPAR:
         """ New paragraph """
     class TAB_OPEN:
@@ -45,109 +42,66 @@ class lexer:
         """ Table close |} """
     class WHITESPACE:
         """ Whitespace class. """
+    class EQUAL_SIGN:
+        """ equal sign for movies and madness \o/ """
+    class APOSTROPHE:
+        """ ', right """
      
     def __init__(self, string):
         self.wikipwn = (a for a in string)
     
     def lexer(self):
         text = ''
-        try:
-            c = self.getchar()
-            while True:
-                if (c == '['):
+        c = self.getchar()
+        while True:
+            if (c in ('[', ']', '{', '}', '<', '>', '=', '\'')):
+                if text:
+                    yield (lexer.TEXT, text)
+                    text = ''
+                num = 1
+                t = self.getchar()
+                while (t == c):
                     t = self.getchar()
-                    if (t == '['):
-                        if text:
-                            yield (lexer.TEXT, text)
-                            text = ''
-                        yield (lexer.WL_OPEN, None)
-                        c = self.getchar()
-                    else:
-                        text = text + c
-                        c = t
-                elif (c == ']'):
-                    t = self.getchar()
-                    if (t == ']'):
-                        if text:
-                            yield (lexer.TEXT, text)
-                            text = ''
-                        yield (lexer.WL_CLOSE, None)
-                        c = self.getchar()
-                    else:
-                        text = text + c
-                        c = t
-                elif (c == '{'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
+                    num += 1
+                if   (c == '['): yield (lexer.SQRE_OPEN,  num)
+                elif (c == ']'): yield (lexer.SQRE_CLOSE, num)
+                elif (c == '{'): yield (lexer.CURL_OPEN,  num)
+                elif (c == '}'): yield (lexer.CURL_CLOSE, num)
+                elif (c == '<'): yield (lexer.ANGL_OPEN,  num)
+                elif (c == '>'): yield (lexer.ANGL_CLOSE, num)
+                elif (c == '='): yield (lexer.EQUAL_SIGN, num)
+                elif (c == '\''): yield(lexer.APOSTROPHE, num)
+                c = t
+            elif (c == '|'):
+                if text:
+                    yield (lexer.TEXT, text)
+                    text = ''
+                t = self.getchar()
+                if (t == '-'):
+                    yield (lexer.TAB_NEWLINE, None)
                     c = self.getchar()
-                    if (c == '|'): # begin table
-                        yield (lexer.TAB_OPEN, None)
-                        c = self.getchar()
-                        continue #at the next while loop
-                    num = 1
-                    while (c == '{'):
-                        c = self.getchar()
-                        num += 1
-                    yield (lexer.CURL_OPEN, num)
-                elif (c == '}'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    num = 0
-                    while (c == '}'):
-                        self.getchar(c)
-                        num += 1
-                    yield (lexer.CURL_CLOSE, num)
-                elif (c == '<'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    yield (lexer.ANGL_OPEN, None)
+                elif (t == '}'):
+                    yield (lexer.TAB_CLOSE, None)
                     c = self.getchar()
-                elif (c == '>'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    yield (lexer.ANGL_OPEN, None)
-                    c = self.getchar()
-                elif (c == '|'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    t = self.getchar()
-                    if (t == '-'):
-                        yield (lexer.TAB_NEWLINE, None)
-                        c = self.getchar()
-                    elif (t == '}'):
-                        yield (lexer.TAB_CLOSE, None)
-                        c = self.getchar()
-                    else:
-                        yield (lexer.PIPE, None)
-                        c = t
-                elif (c == ' ' or c == '\t'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    yield (lexer.WHITESPACE, None)
-                    while (c == ' ' or c == '\t'):
-                        c = self.getchar()   #eat up remaining spaces
-                elif (c == '\n'):
-                    if text:
-                        yield (lexer.TEXT, text)
-                        text = ''
-                    t = self.getchar()
-                    while (t == ' '):
-                        t = self.getchar()  #ignore any spaces
-                    if (t == '\n'): #new paragraph
-                        yield (lexer.NEWPAR, None)
-                    while (c == '\n'): #eat up remaining newlines
-                        c = self.getchar()
                 else:
-                    text = text + c
-                    c = self.getchar()
-        except OutOfCharsException:
-            pass
+                    yield (lexer.PIPE, None)
+                    c = t
+            elif re.match('\s', c): # whitespace eater pro (TM)
+                if text:
+                    yield (lexer.TEXT, text)
+                    text = ''
+                ws = c
+                c = self.getchar()
+                while re.match('\s', c):
+                    ws += c
+                    c = self.getchar()   #eat up remaining whitespace
+                if (ws.count('\n') > 1):
+                    yield (lexer.NEWPAR, ws)
+                else:
+                    yield (lexer.WHITESPACE, ws)
+            else:
+                text = text + c
+                c = self.getchar()
 
     def getchar(self): 
         return self.wikipwn.next()
