980f1ef766b1892dfb50cee03ec754728aa7a7c7
btongminh <btongminh@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-07-10 12:25:15 +0000

    Only exit once the jobQueue has been emptied.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3865 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- threadpool.py
+++ threadpool.py
@@ -28,137 +28,138 @@ __version__ = '$Id$'
 import sys, threading, os
  
 class ThreadPool(dict):
-    pools = []
-    def __init__(self, worker):
-        dict.__init__(self)
+	pools = []
+	def __init__(self, worker):
+		dict.__init__(self)
  
-        self.jobLock = threading.Lock()
-        self.jobQueue = []
-        self.worker = worker
-        self.threads = []
+		self.jobLock = threading.Lock()
+		self.jobQueue = []
+		self.worker = worker
+		self.threads = []
  
-        self.pools.append(self)
+		self.pools.append(self)
  
-    def append(self, job):
-        self.jobLock.acquire()
-        try:
-            self.jobQueue.append(job)
-            unlock_workers = len(self.jobQueue)
-     
-            counter = 0
-            for event in self.itervalues():
-                if not event.isSet():
-                    event.set()
-                    counter += 1
-                if counter == unlock_workers:
-                    break
-        finally:
-            self.jobLock.release()
+	def append(self, job):
+		self.jobLock.acquire()
+		try:
+			self.jobQueue.append(job)
+			unlock_workers = len(self.jobQueue)
+	 
+			counter = 0
+			for event in self.itervalues():
+				if not event.isSet():
+					event.set()
+					counter += 1
+				if counter == unlock_workers:
+					break
+		finally:
+			self.jobLock.release()
  
-    def add_thread(self, *args, **kwargs):
-        self.jobLock.acquire()
-        try:
-            thread = self.worker(self, *args, **kwargs)
-            self.threads.append(thread)
-            self[id(thread)] = threading.Event()
-        finally:
-            self.jobLock.release()
+	def add_thread(self, *args, **kwargs):
+		self.jobLock.acquire()
+		try:
+			thread = self.worker(self, *args, **kwargs)
+			self.threads.append(thread)
+			self[id(thread)] = threading.Event()
+		finally:
+			self.jobLock.release()
  
-    def start(self):
-        for thread in self.threads:
-            if not thread.isAlive():
-                thread.start()
-    def exit(self):
-        self.jobLock.acquire()
-        try:
-            del self.jobQueue[:]
-            for thread in self.threads:
-                thread.quit = True
-                self[id(thread)].set()
-        finally:
-            self.jobLock.release()
+	def start(self):
+		for thread in self.threads:
+			if not thread.isAlive():
+				thread.start()
+	def exit(self):
+		self.jobLock.acquire()
+		try:
+			del self.jobQueue[:]
+			for thread in self.threads:
+				thread.quit = True
+				self[id(thread)].set()
+		finally:
+			self.jobLock.release()
  
 class Thread(threading.Thread):
-    def __init__(self, pool):
-        threading.Thread.__init__(self)
-        self.pool = pool
-        self.quit = False
+	def __init__(self, pool):
+		threading.Thread.__init__(self)
+		self.pool = pool
+		self.quit = False
  
-    def run(self):
-	while True:
-		# No try..finally: lock.release() here:
-		# The lock might be released twice, in case
-		# the thread waits for an event, a race 
-		# condition might occur where a lock is released
-		# that is acquired by another thread. 
+	def run(self):
+		while True:
+			# No try..finally: lock.release() here:
+			# The lock might be released twice, in case
+			# the thread waits for an event, a race 
+			# condition might occur where a lock is released
+			# that is acquired by another thread. 
+			self.pool.jobLock.acquire()
+	
+			if self.quit and not self.pool.jobQueue:
+				# Only return once the jobQueue is empty.
+				self.pool.jobLock.release()
+				return
+		
+			if not self.pool.jobQueue:
+				# In case no job is available, wait for the pool 
+				# to  call and do not start a busy while loop.
+				event = self.pool[id(self)]
+				self.pool.jobLock.release()
+				event.clear()
+				event.wait()
+				continue
+			job = self.pool.jobQueue.pop(0)
+			self.pool.jobLock.release()
+		
+			self.do(job)
+ 
+	def exit(self):
 		self.pool.jobLock.acquire()
-
-		if self.quit:
+		try:
+			self.quit = True
+			self.pool[id(self)].set()
+		finally:
 			self.pool.jobLock.release()
-			return
-	
-		if not self.pool.jobQueue:
-			# In case no job is available, wait for the pool 
-			# to  call and do not start a busy while loop.
-			event = self.pool[id(self)]
-			self.pool.jobLock.release()
-			event.clear()
-			event.wait()
-			continue
-		job = self.pool.jobQueue.pop(0)
-		self.pool.jobLock.release()
-	
-		self.do(job)
- 
-    def exit(self):
-        self.pool.jobLock.acquire()
-        try:
-            self.quit = True
-            self.pool[id(self)].set()
-        finally:
-            self.pool.jobLock.release()
  
 def catch_signals():
-    import signal
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
+	import signal
+	signal.signal(signal.SIGINT, sig_handler)
+	signal.signal(signal.SIGTERM, sig_handler)
  
 def sig_handler(signalnum, stack):
-        import signal
-        for pool in ThreadPool.pools:
-                pool.exit()
-                
-        if signalnum == signal.SIGINT:
-                raise KeyboardInterrupt
-        if signalnum == signal.SIGTERM:
-                raise SystemExit
-                
+		import signal
+		for pool in ThreadPool.pools:
+				pool.exit()
+				
+		if signalnum == signal.SIGINT:
+				raise KeyboardInterrupt
+		if signalnum == signal.SIGTERM:
+				raise SystemExit
+				
 def terminate():
-        # Maybe not a good idea, will also kill child processes
+	# Maybe not a good idea, will also kill child processes
 	import signal
-        os.kill(0, signal.SIGTERM)
+	os.kill(0, signal.SIGTERM)
  
 if __name__ == '__main__':
-    import time
-    # Test cases
+	import time
+	# Test cases
  
-    class Worker(Thread):
-        def do(self, args):
-            print 'Working', self
-            time.sleep(10)
-            print 'Done', self
+	class Worker(Thread):
+		def do(self, args):
+			print 'Working', self
+			time.sleep(10)
+			print 'Done', self
  
-    pool = ThreadPool(Worker)
-    print 'Spawning 5 threads'
-    [pool.add_thread() for i in xrange(5)]
-    pool.start()
+	pool = ThreadPool(Worker)
+	print 'Spawning 5 threads'
+	[pool.add_thread() for i in xrange(5)]
+	pool.start()
  
-    print 'Doing 25 jobs'
-    for i in xrange(25):
-        print 'Job', i
-        pool.append(i)
-        time.sleep(i % 6)
+	print 'Doing 25 jobs'
+	for i in xrange(25):
+		print 'Job', i
+		pool.append(i)
+		time.sleep(i % 6)
  
-    for thread in pool.threads:
-        thread.exit()
+	for thread in pool.threads:
+		thread.exit()
 
