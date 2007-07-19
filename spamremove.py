6dd5da5e8e38245037f805f629e747d017411224
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-07-19 00:52:56 +0000

    added de: translation added main() method to prepare refactoring fixed indentation (3 spaces were used) added some color markup
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@3884 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- spamremove.py
+++ spamremove.py
@@ -21,60 +21,67 @@ Command line options:
 
 """
 
+def main():
+    automatic = False
+    msg = {
+        'de': u'Entferne in Spam-Whitelist eingetragenen Weblink auf %s',
+        'en': u'Removing links to spammed site %s',
+        'nl': u'Links naar gespamde site %s verwijderd',
+    }
+    spamSite = ''
+    for arg in wikipedia.handleArgs():
+        if arg.startswith("-automatic"):
+            automatic = True
+        else:
+            spamSite = arg
+    if not automatic:
+        wikipedia.put_throttle.setDelay(1)
+    if not spamSite:
+        wikipedia.output(u"No spam site specified.")
+        sys.exit()
+    mysite = wikipedia.getSite()
+    pages = list(set(mysite.linksearch(spamSite)))
+    wikipedia.getall(mysite, pages)
+    for p in pages:
+        if not spamSite in p.get():
+            continue
+        wikipedia.output(u'')
+        # Show the title of the page where the link was found.
+        # Highlight the title in purple.
+        colors = [None] * 6 + [13] * len(p.title()) + [None] * 4
+        wikipedia.output(u"\n\n>>> %s <<<" % p.title(), colors = colors)
+        lines = p.get().split('\n')
+        newpage = []
+        lastok = ""
+        for line in lines:
+            if spamSite in line:
+                if lastok:
+                    wikipedia.output(lastok)
+                wikipedia.output(line, colors = [12] * len(line))
+                lastok = None
+            else:
+                newpage.append(line)
+                if line.strip():
+                    if lastok is None:
+                        wikipedia.output(line)
+                    lastok = line
+        if automatic:
+            answer = "Y"
+        else:
+            answer = "blablabla"
+        while not answer in "yYnNeE":
+            answer = wikipedia.input("Delete the red lines ([Y]es, [N]o, [E]dit)?")[0]
+        if answer in "nN":
+            continue
+        elif answer in "eE":
+            editor = editarticle.TextEditor()
+            newtext = editor.edit(p.get(), highlight = spamSite)
+        else:
+            newtext = "\n".join(newpage)
+        if newtext != p.get():
+            p.put(newtext, wikipedia.translate(mysite, msg) % spamSite)
+
 try:
-   automatic = False
-   msg = {'en': 'Removing links to spammed site %s',
-          'nl': 'Links naar gespamde site %s verwijderd',
-          }
-   site = ''
-   for arg in wikipedia.handleArgs():
-      if arg.startswith("-automatic"):
-         automatic = True
-      else:
-         site = arg
-   if not automatic:
-      wikipedia.put_throttle.setDelay(1)
-   if not site:
-      print "No site specified."
-      sys.exit()
-   mysite = wikipedia.getSite()
-   pages = list(set(mysite.linksearch(site)))
-   wikipedia.getall(mysite,pages)
-   for p in pages:
-      if not site in p.get():
-         continue
-      print
-      wikipedia.output(u"=== [[%s]] ==="%p.title())
-      lines = p.get().split('\n')
-      newpage = []
-      lastok = ""
-      for line in lines:
-         if site in line:
-            if lastok:
-               wikipedia.output(lastok)
-            wikipedia.output(line,colors = [12] * len(line))
-            lastok = None
-         else:
-            newpage.append(line)
-            if line.strip():
-               if lastok is None:
-                  wikipedia.output(line)
-               lastok = line
-      if automatic:
-         answer = "Y"
-      else:
-         answer = "blablabla"
-      while not answer in "yYnNeE":
-         answer = wikipedia.input("Delete the red lines ([Y]es, [N]o, [E]dit)?")[0]
-      if answer in "nN":
-         continue
-      elif answer in "eE":
-         editor = editarticle.TextEditor()
-         newtext = editor.edit(p.get(), highlight = site)
-      else:
-         newtext = "\n".join(newpage)
-      if newtext != p.get():
-         p.put(newtext, wikipedia.translate(mysite, msg) % site)
-   
+    main()
 finally:
    wikipedia.stopme()
