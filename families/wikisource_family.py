7c86bb47a1d8cab4e1c84ba8f5ef5ea39c1c27c8
misza13 <misza13@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-08-09 16:14:13 +0000

    Fix bug #1770747 - main wikisource.org wiki (no domain prefix) will use '-' as language key.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4020 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wikisource_family.py
+++ families/wikisource_family.py
@@ -11,7 +11,7 @@ class Family(family.Family):
         self.name = 'wikisource'
        
         self.langs = {
-            '':'wikisource.org',
+            '-':'wikisource.org',
             'dk':'da.wikisource.org',
             'jp':'ja.wikisource.org',
             'minnan':'zh-min-nan.wikisource.org',
