b509313234d1846faca4aef49e149f834a9e7ab5
misza13 <misza13@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-09-03 17:10:53 +0000

    Fixed configuration updating regex.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4189 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- archivebot.py
+++ archivebot.py
@@ -386,7 +386,7 @@ class PageArchiver(object):
                 self.archives[a].update(comment)
 
             #Save the page itself
-            self.Page.header = re.sub('{{'+self.tpl+'\n[^}]*\n}}',self.attr2text(),self.Page.header)
+            self.Page.header = re.sub('{{'+self.tpl+'\n.*?\n}}',self.attr2text(),self.Page.header,re.DOTALL)
             self.commentParams['count'] = self.archivedThreads
             self.commentParams['archives'] = ', '.join(['[['+a.title+']]' for a in self.archives.values()])
             if not self.commentParams['archives']:
