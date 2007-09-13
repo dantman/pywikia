ecc1724cb01b5f913c7f8af0a51e7ef4a7786113
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-09-13 18:23:06 +0000

    bugfix
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4276 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -4018,7 +4018,7 @@ Maybe the server is down. Retrying in %i minutes..."""
         # A redirect starts with hash (#), followed by a keyword, then
         # arbitrary stuff, then a wikilink. The wikilink may contain
         # a label, although this is not useful.
-        return re.compile(r'# *' + redirKeywordsR + '.*?\[\[(.*?)(?:\|.*?)\]\]', re.IGNORECASE | re.UNICODE | re.DOTALL)
+        return re.compile(r'# *' + redirKeywordsR + '.*?\[\[(.*?)(?:\|.*?)?\]\]', re.IGNORECASE | re.UNICODE | re.DOTALL)
 
     # The following methods are for convenience, so that you can access
     # methods of the Family class easier.
