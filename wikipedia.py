b81b487fbb675d0efb794bc1e1e9965faab97dad
wikipedian <wikipedian@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-09-21 01:21:51 +0000

    hack to enable windows users of replace.py to use linebreaks
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4337 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -2656,6 +2656,15 @@ def replaceExcept(text, old, new, exceptions, caseInsensitive = False, allowover
         else:
             # We found a valid match. Replace it.
 
+            # This is a hack for Windows users. As they're unable to enter real
+            # linebreaks in the command prompt, this line allows them to enter
+            # backslash-n instead.
+            # This means that it is impossible replace a string with a real
+            # backslash-n sequence (it will always become a linebreak), but
+            # it is unprobable that someone will want to do that.
+            if sys.platform=='win32':
+                new = new.replace('\\n', '\n')
+
             # We cannot just insert the new string, as it may contain regex
             # group references such as \2 or \g<name>.
             # On the other hand, this approach does not work because it can't
