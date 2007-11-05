060b89c593c6517c1013453d61b595d437d6cf0f
warddr <warddr@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-11-05 16:39:03 +0000

    +namespacess
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4508 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wikibond_family.py
+++ families/wikibond_family.py
@@ -3,15 +3,20 @@
 import family
 
 # I added this becouse someone asked me to. The url op the wiki:  nl.wikibond.org
-#to do: add namespaces
 
 class Family(family.Family):
-    
+   
     def __init__(self):
         family.Family.__init__(self)
         self.name = 'wikibond'
         self.langs = {
             'nl': 'nl.wikibond.org',
+        }
+        self.namespaces[4] = {
+            'nl': [u'WikiBond'],
+        }
+        self.namespaces[5] = {
+            'botwiki': [u'Overleg WikiBond'],
         }
 
     def path(self, code):
