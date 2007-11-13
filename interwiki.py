97aa1489c7e948698be0e37de8f16e479cf9277d
leogregianin <leogregianin@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-11-13 15:42:50 +0000

    [1824150] Corrections to Georgian text by André Malafaya Baptista
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4533 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- interwiki.py
+++ interwiki.py
@@ -302,7 +302,7 @@ msg = {
     'is': (u'robot ', u'Bæti við', u'Fjarlægi', u'Breyti'),
     'it': (u'robot ', u'Aggiungo', u'Tolgo', u'Modifico'),
     'ja': (u'ロボットによる ', u'追加', u'変更', u'除去'),
-    'ka': (u'რობოტი ', u'დამატება', u'წაშლა', u'შეცვლა'),
+    'ka': (u'ბოტის ', u'დამატება', u'წაშლა', u'შეცვლა'),
     'ko': (u'로봇이 ', u'더함', u'지움', u'바꿈'),
     'kk': (u'Боттың ', u'үстегені', u'аластағаны', u'түзеткені'),
     'ksh': (u'Bot: ', u'dobëijedonn', u'erußjenumme', u'ußjewääßelt'),
