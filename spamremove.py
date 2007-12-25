0a1073e90ca1c922472de12f3e708a1442ee1089
borgo <borgo@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2007-12-25 15:15:04 +0000

    Adding it summary traslation
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@4758 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- spamremove.py
+++ spamremove.py
@@ -31,6 +31,7 @@ def main():
         'de': u'Entferne in Spam-Blacklist eingetragenen Weblink auf %s',
         'en': u'Removing links to spammed site %s',
         'he': u'מסיר קישורים לאתר ספאם %s',
+        'it': u'Rimuovo link contenuto nella Spam-Blacklist %s',
         'ja': u'ロボットによる: 迷惑リンク削除 %s',
         'nl': u'Links naar gespamde site: %s verwijderd',
         'pt': u'Removendo links de spam do site %s',
