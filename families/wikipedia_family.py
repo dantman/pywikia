74d57239501c4296e33439344d29eff60c2b459c
borgo <borgo@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-05-06 00:17:45 +0000

    Added indonesian disambig template
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5312 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wikipedia_family.py
+++ families/wikipedia_family.py
@@ -511,6 +511,7 @@ class Family(family.Family):
             'hu':  [u'Egyert', u'Disambig', u'Egyért'],
             'hy':  [u'Երկիմաստ', u'Disambig'],
             'ia':  [u'Disambiguation', u'Disambig'],
+            'id':  [u'Disingkat',u'Disambig'],
             'io':  [u'Homonimo', u'Disambig'],
             'is':  [u'Aðgreining', u'Disambig'],
             'it':  [u'Disambigua', u'Sigla2', u'Sigla3', u'Sigla4', u'Cogni'],
