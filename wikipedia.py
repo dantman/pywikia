95ecad1c768ba2896683b9a77d062d36284944fd
nicdumz <nicdumz@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-05-10 23:57:26 +0000

    typo, sorry :)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5353 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -1401,7 +1401,7 @@ not supported by PyWikipediaBot!"""
             # I think the error message title was changed from "Wikimedia Error"
             # to "Wikipedia has a problem", but I'm not sure. Maybe we could
             # just check for HTTP Status 500 (Internal Server Error)?
-            if ("<title>Wikimedia Error</title>" in data or "has a problem</title>" in data) \:
+            if ("<title>Wikimedia Error</title>" in data or "has a problem</title>" in data) \
                 or response.status == 500:
                 output(
                 u"Wikimedia has technical problems; will retry in %i minute%s."
