65a5d360fdb2f0b9c518085ef310c7ad45ff3efa
filnik <filnik@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-05-16 13:16:57 +0000

    Forgot one, thanks Multichil :)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5383 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- checkimages.py
+++ checkimages.py
@@ -306,7 +306,7 @@ comm10 = {
 # because they are already put in the regex).
 HiddenTemplate = {
         'commons':[u'information', u'trademarked', u'trademark', u'uncategorized[ _]image', u'needscategory',
-                   u'dupe', u'duplicate', u'uncat', u'uncategorized', u'watermark'],
+                   u'dupe', u'duplicate', u'uncat', u'uncategorized', u'watermark', u'nocat'],
         'de':[u'information'],
         'en':[u'information'],
         'it':[u'edp', u'informazioni[ _]file', u'information', u'trademark'],
