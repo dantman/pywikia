d332d18e1635504061aafc54bebdaaf07baf3753
cosoleto <cosoleto@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-06-10 10:27:16 +0000

    Fixed bug #1900272 Login Captcha in FreeBSD (login.py)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5548 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- userinterfaces/terminal_interface.py
+++ userinterfaces/terminal_interface.py
@@ -287,8 +287,7 @@ class UI:
             wikipedia.output(u'Opening CAPTCHA in your web browser...')
             webbrowser.open(url)
             return wikipedia.input(u'What is the solution of the CAPTCHA that is shown in your web browser?')
-        except ImportError, e:
-            return wikipedia.input(u'What is the solution of the CAPTCHA at %s ?' % url)
-        except WindowsError:
-            return wikipedia.input(u'What is the solution of the CAPTCHA at %s ?' % url)
+        except:
+            pass
 
+        return wikipedia.input(u'What is the solution of the CAPTCHA at %s ?' % url)
