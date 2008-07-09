0fa390f5cd3d49f9cade056ba721e0aec8a9a786
mfarag <mfarag@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-07-09 06:58:35 +0000

    Update Arabic (ar)
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5697 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- clean_sandbox.py
+++ clean_sandbox.py
@@ -25,7 +25,7 @@ import wikipedia
 import time
 
 content = {
-    'ar': u'{{من فضلك اترك هذا السطر ولا تعدله (عنوان ساحة اللعب)}}\n<!-- مرحبا! خذ راحتك في تجربة مهارتك في التنسيق والتحرير أسفل هذا السطر. هذه الصفحة لتجارب التعديل ، سيتم تفريغ هذه الصفحة كل 6 ساعات. -->',
+    'ar': u'{{من فضلك اترك هذا السطر ولا تعدله (عنوان ساحة التجربة)}}\n<!-- مرحبا! خذ راحتك في تجربة مهارتك في التنسيق والتحرير أسفل هذا السطر. هذه الصفحة لتجارب التعديل ، سيتم تفريغ هذه الصفحة كل 6 ساعات. -->',
     'da': u'{{subst:Sandkasse tekst}}',
     'de': u'{{Bitte erst NACH dieser Zeile schreiben! (Begrüßungskasten)}}\r\n',
     'en': u'{{Please leave this line alone (sandbox heading)}}\n <!-- Hello! Feel free to try your formatting and editing skills below this line. As this page is for editing experiments, this page will automatically be cleaned every 12 hours. -->',
@@ -48,7 +48,7 @@ content = {
     }
 
 msg = {
-    'ar': u'روبوت: هذه الصفحة سيتم تفريغها تلقائياً',
+    'ar': u'روبوت: هذه الصفحة سيتم تفريغها تلقائيا',
     'da': u'Bot: Nyt sand',
     'de': u'Bot: Setze Seite zurück.',
     'en': u'Robot: This page will automatically be cleaned.',
@@ -70,7 +70,7 @@ msg = {
     }
 
 sandboxTitle = {
-    'ar': u'ويكيبيديا:ساحة اللعب',
+    'ar': u'ويكيبيديا:ساحة التجربة',
     'da': u'Wikipedia:Sandkassen',
     'de': u'Wikipedia:Spielwiese',
     'en': u'Wikipedia:Sandbox',
