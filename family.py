5d970b4c365b1675726e9e07459f56922268c909
nicdumz <nicdumz@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-07-28 13:18:32 +0000

    ident fix
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5767 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- family.py
+++ family.py
@@ -3185,11 +3185,11 @@ class Family:
         return True
     
     def post_get_convert(self, site, getText):
-	"""Does a conversion on the retrieved text from the wiki
-	   i.e. Esperanto X-conversion """
-	return getText
+    	"""Does a conversion on the retrieved text from the wiki
+	    i.e. Esperanto X-conversion """
+	    return getText
 
     def pre_put_convert(self, site, putText):
-	"""Does a conversion on the text to insert on the wiki
-	   i.e. Esperanto X-conversion """
-	return putText
+	    """Does a conversion on the text to insert on the wiki
+	    i.e. Esperanto X-conversion """
+	    return putText
