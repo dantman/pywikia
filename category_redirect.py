bf3f77deda4c8f0014447467b2289d51ca950a26
multichill <multichill@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-08-21 20:52:15 +0000

    Added cooldown period.
    We want to wait several days after the last edit before we start moving things around. This it to prevent edit wars and vandals.
    
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5831 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- category_redirect.py
+++ category_redirect.py
@@ -15,9 +15,13 @@ Moves all images, pages and categories in redirect categories to the target cate
 
 import wikipedia, config, catlib
 from category import *
+from datetime import datetime
+from datetime import timedelta
 
 redirect_templates = [u'Category redirect', u'Categoryredirect', u'See cat', u'Seecat', u'Catredirect', u'Cat redirect', u'CatRed', u'Catredir']
 move_message = u'Moving from [[%s|%s]] to [[%s|%s]] (following [[Template:Category redirect|category redirect]])'
+cooldown = 7 # days
+
 
 def get_redirect_cat(category=None):
     '''
@@ -33,6 +37,15 @@ def get_redirect_cat(category=None):
                 return None
     return destination
     
+def readyToEdit(old_category):
+    '''
+    If the category is edited more recenty than cooldown, return false, otherwise true.
+    '''
+    dateformat ="%Y%m%d%H%M%S"
+    today = datetime.now()
+    deadline = today + timedelta(days=-cooldown)
+    old_category.get()
+    return (deadline.strftime(dateformat) > old_category.editTime())
 
 def main():
     '''
@@ -45,24 +58,27 @@ def main():
     catbot = None
     
     for old_category in dirtycat.subcategories():
-        destination = get_redirect_cat(old_category)
-        if destination:
-            wikipedia.output(destination.title())
-            for page in old_category.articles():
-                try:                                
-                    catlib.change_category(page, old_category, destination, move_message % (old_category.title(), old_category.titleWithoutNamespace(), destination.title(), destination.titleWithoutNamespace()))
-                except wikipedia.IsRedirectPage:
-                    wikipedia.output(page.title() + u' is a redirect!')
-            for cat in old_category.subcategories():
-                try:                
-                    catlib.change_category(cat, old_category, destination, move_message % (old_category.title(), old_category.titleWithoutNamespace(), destination.title(), destination.titleWithoutNamespace()))
-                except wikipedia.IsRedirectPage:
-                    wikipedia.output(page.title() + u' is a redirect!')
-        #Dummy edit to refresh the page, shouldnt show up in any logs.
-        try:
-            old_category.put(old_category.get())
-        except:
-            wikipedia.output(u'Dummy edit at ' + old_category.title() + u' failed')        
+        #We want to wait several days after the last edit before we start moving things around.
+        #This it to prevent edit wars and vandals.
+        if(readyToEdit(old_category)):
+            destination = get_redirect_cat(old_category)
+            if destination:
+                wikipedia.output(destination.title())
+                for page in old_category.articles():
+                    try:                                
+                        catlib.change_category(page, old_category, destination, move_message % (old_category.title(), old_category.titleWithoutNamespace(), destination.title(), destination.titleWithoutNamespace()))
+                    except wikipedia.IsRedirectPage:
+                        wikipedia.output(page.title() + u' is a redirect!')
+                for cat in old_category.subcategories():
+                    try:                
+                        catlib.change_category(cat, old_category, destination, move_message % (old_category.title(), old_category.titleWithoutNamespace(), destination.title(), destination.titleWithoutNamespace()))
+                    except wikipedia.IsRedirectPage:
+                        wikipedia.output(page.title() + u' is a redirect!')
+            #Dummy edit to refresh the page, shouldnt show up in any logs.
+            try:
+                old_category.put(old_category.get())
+            except:
+                wikipedia.output(u'Dummy edit at ' + old_category.title() + u' failed')        
 
 if __name__ == "__main__":
     try:
