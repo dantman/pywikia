f73c12da155057b6de8783ec1b9843fe5059532a
purodha <purodha@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-09-19 09:27:33 +0000

    cu wikipedia localization update.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5905 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wikipedia_family.py
+++ families/wikipedia_family.py
@@ -69,7 +69,8 @@ class Family(family.Family):
             'crh': u'Vikipediya',
             'cs': u'Wikipedie',
             'csb': u'Wiki',
-            'cu': u'Википедї',
+            'cu': [u'Википедї', u'Википє́дїꙗ']
+            # 'cu': u'Википє́дїꙗ'
             'cv': u'Википеди',
             'cy': u'Wicipedia',
             'dsb': u'Wikipedija',
@@ -166,7 +167,8 @@ class Family(family.Family):
             'crh': [u'Vikipediya muzakeresi', u'Vikipediya музакереси'],
             'cs': u'Wikipedie diskuse',
             'csb': u'Diskùsëjô Wiki',
-            'cu': u'Википедїѩ бєсѣ́да',
+            'cu': [u'Википедїѩ бєсѣ́да', u'Википє́дїѩ_бєсѣ́да']
+            # 'cu': u'Википє́дїѩ_бєсѣ́да'
             'cv': u'Википеди сӳтсе явмалли',
             'cy': u'Sgwrs Wicipedia',
             'da': u'Wikipedia-diskussion',
