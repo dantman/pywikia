0d49ef9f21a335dd5e049cc25a8a5ac2261ae95c
nicdumz <nicdumz@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2008-10-18 14:01:08 +0000

    Updating tranwiki family file
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@5990 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/i18n_family.py
+++ families/i18n_family.py
@@ -21,6 +21,12 @@ class Family(family.Family):
         self.namespaces[5] = {
             '_default': [u'Betawiki talk'],
         }
+        self.namespaces[6] = {
+            '_default': [u'File'],
+        }
+        self.namespaces[7] = {
+            '_default': [u'File talk'],
+        }
         self.namespaces[100] = {
             '_default': [u'Portal'],
         }
@@ -32,6 +38,48 @@ class Family(family.Family):
         }
         self.namespaces[1103] = {
             '_default': [u'Translating talk'],
+        }
+        self.namespaces[1200] = {
+            '_default': [u'Voctrain'],
+        }
+        self.namespaces[1201] = {
+            '_default': [u'Voctrain talk'],
+        }
+        self.namespaces[1202] = {
+            '_default': [u'FreeCol'],
+        }
+        self.namespaces[1203] = {
+            '_default': [u'FreeCol talk'],
+        }
+        self.namespaces[1204] = {
+            '_default': [u'Deprecated1'],
+        }
+        self.namespaces[1205] = {
+            '_default': [u'Deprecated1 talk'],
+        }
+        self.namespaces[1206] = {
+            '_default': [u'Deprecated2'],
+        }
+        self.namespaces[1207] = {
+            '_default': [u'Deprecated2 talk'],
+        }
+        self.namespaces[1208] = {
+            '_default': [u'Zabbix'],
+        }
+        self.namespaces[1209] = {
+            '_default': [u'Zabbix talk'],
+        }
+        self.namespaces[1210] = {
+            '_default': [u'Mantis'],
+        }
+        self.namespaces[1211] = {
+            '_default': [u'Mantis talk'],
+        }
+        self.namespaces[9900] = {
+            '_default': [u'Translations'],
+        }
+        self.namespaces[9901] = {
+            '_default': [u'Translations talk'],
         }
 
     def version(self, code):
