221f351d9f3b949ad5ba0101809952ada3fd1aed
multichill <multichill@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-01-09 20:23:10 +0000

    Ignore some templates at fr.
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6242 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- commonscat.py
+++ commonscat.py
@@ -156,6 +156,7 @@ commonscatTemplates = {
 ignoreTemplates = {
     'ar' : [u'تحويلة تصنيف', u'كومنز', u'كومونز', u'Commonscat'],
     'en' : [u'Category redirect', u'Commons', u'Commonscat', u'Commonscat1A', u'Commoncats', u'Commonscat4Ra', u'Sisterlinks', u'Sisterlinkswp', u'Tracking category', u'Template category', u'Wikipedia category'],
+    'fr' : [u'Commons', u'Commons cat'],
     'it' : [(u'Ip', 'commons='), ('Interprogetto', 'commons=')],
     'ja' : [u'CommonscatS'],
     'la' : [u'Categoria Nati', u'Categoria Mortui'],
