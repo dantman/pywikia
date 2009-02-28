1d6273c845b7237b3d68012ee0cc87681a9b56b4
multichill <multichill@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-02-28 17:06:03 +0000

    +Søsterlinks
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6454 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- commonscat.py
+++ commonscat.py
@@ -155,7 +155,7 @@ commonscatTemplates = {
 
 ignoreTemplates = {
     'ar' : [u'تحويلة تصنيف', u'كومنز', u'كومونز', u'Commonscat'],
-    'da' : [u'Commons', u'Commons left', u'Commons2', u'Commons3', u'Commonsbilleder', u'Commonscat left', u'Commonscat2', u'Commonsmedia', u'GalleriCommons',],
+    'da' : [u'Commons', u'Commons left', u'Commons2', u'Commons3', u'Commonsbilleder', u'Commonscat left', u'Commonscat2', u'Commonsmedia', u'GalleriCommons', u'Søsterlinks'],
     'en' : [u'Category redirect', u'Commons', u'Commonscat', u'Commonscat1A', u'Commoncats', u'Commonscat4Ra', u'Sisterlinks', u'Sisterlinkswp', u'Tracking category', u'Template category', u'Wikipedia category'],
     'fr' : [u'Commons', u'Commons cat'],
     'it' : [(u'Ip', 'commons='), ('Interprogetto', 'commons=')],
