1a0baa79ee757305ddec7a9b6c65b46cecb22ae4
nicdumz <nicdumz@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-04-14 16:55:58 +0000

    [1999787] "InvocationException in interwiki_graph.py" See http://www.graphviz.org/doc/info/lang.html : dot does not allow ':' in alphanumeric IDs. The only way to include those is to pass to dot a double quoted string.
    Credits go to mpupat for tracking down the issue
    
    
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6605 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- interwiki_graph.py
+++ interwiki_graph.py
@@ -46,11 +46,11 @@ class GraphDrawer:
         self.subject = subject
 
     def getLabel(self, page):
-        return (u'"%s:%s"' % (page.site().language(), page.title())).encode('utf-8')
+        return (u'"\"%s:%s\""' % (page.site().language(), page.title())).encode('utf-8')
 
     def addNode(self, page):
         node = pydot.Node(self.getLabel(page), shape = 'rectangle')
-        node.set_URL('http://%s%s' % (page.site().hostname(), page.site().get_address(page.urlname())))
+        node.set_URL("\"http://%s%s\"" % (page.site().hostname(), page.site().get_address(page.urlname())))
         node.set_style('filled')
         node.set_fillcolor('white')
         node.set_fontsize('11')
