dantman/pywikia

commit 6dd354809127cbecf9ee40492ab2aba4bce30e33

Removed a workaround for the Live Search words number problem, useless now and in the future.
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6600 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

copyright.py
@@ -1191,11 +1191,7 @@ def main():
 
 excl_list = exclusion_list()
 
-if number_of_words > 22:
-    if not config.copyright_google and not config.copyright_yahoo and config.copyright_msn:
-        warn("'number_of_words' variable set to 22 as Live Search requires a lower value of %s" % number_of_words, prefix = 'Warning')
-        number_of_words = 22
-    elif config.copyright_msn:
+if number_of_words > 22 and config.copyright_msn:
         warn("Live Search requires a lower value for 'number_of_words' variable "
              "(current value is %d, a good value may be 22)." % (number_of_words), prefix = 'Warning')
 

commit 1a2fb434a585538e0f01ad400518b27b2c4f272b

Fix regression introduced from r6600 when the -text command line parameter is used.
git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6649 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

copyright.py
@@ -1191,7 +1191,7 @@ def main():
         wikipedia.output(__doc__, 'utf-8')
 
     if text:
-        output = query(lines = text.splitlines())
+        output = SearchEngine().query(lines = text.splitlines())
         if output:
             wikipedia.output(output)
 