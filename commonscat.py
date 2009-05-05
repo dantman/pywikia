af2ae90a4b5e6013d9f31e1ba9ec223a25b25f4c
siebrand <siebrand@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-05-05 18:39:13 +0000

    Update ignoreTemplates for 'da'
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@6833 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- commonscat.py
+++ commonscat.py
@@ -157,7 +157,7 @@ ignoreTemplates = {
     'af' : [u'commonscat', u'commons'],
     'ar' : [u'تحويلة تصنيف', u'كومنز', u'كومونز', u'Commonscat'],
     'cs' : [u'Commons', u'commons cat', u'Sestřičky', u'Sisterlinks'],
-    'da' : [u'Commons', u'Commons left', u'Commons2', u'Commons3', u'Commonsbilleder', u'Commonscat left', u'Commonscat2', u'Commonsmedia', u'GalleriCommons', u'Søsterlinks'],
+    'da' : [u'Commons', u'Commons left', u'Commons2', u'Commonsbilleder', u'Commonscat left', u'Commonscat2', u'GalleriCommons', u'Søsterlinks'],
     'en' : [u'Category redirect', u'Commons', u'Commonscat', u'Commonscat1A', u'Commoncats', u'Commonscat4Ra', u'Sisterlinks', u'Sisterlinkswp', u'Tracking category', u'Template category', u'Wikipedia category'],
     'fr' : [u'Commons', u'Commons cat'],
     'it' : [(u'Ip', 'commons='), ('Interprogetto', 'commons=')],
