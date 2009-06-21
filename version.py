582bbd186b632b68f22708466635dd86ce87398a
Daniel Friesen <github@nadir-seen-fire.com>
2009-06-21 02:52:34 -0700

    Modify version.py to grab version info from git

--- version.py
+++ version.py
@@ -6,7 +6,7 @@
 #
 __version__ = '$Id$'
 
-import os
+import os, subprocess
 import time
 import sys
 import wikipediatools
@@ -24,21 +24,24 @@ def getversiondict():
     if cache:
       return cache
     try:
-        (tag, rev, date) = getversion_svn()
+        (tag, rev, date) = getversion_git()
     except Exception, e:
         try:
-            (tag, rev, date) = getversion_nightly()
+            (tag, rev, date) = getversion_svn()
         except Exception, e:
-            import wikipedia
-            d = wikipedia.__version__.split(' ')
-            tag = ''
-            if d == '$Id$':
-                # wikipedia.py got imported without using svn
-                date = 'date unknown'
-                rev = '-1 (rev unknown)' 
-            else:
-                date = time.strptime('T'.join(d[3:5]), '%Y-%m-%dT%H:%M:%SZ')
-                rev = d[2] + ' (wikipedia.py)'
+            try:
+                (tag, rev, date) = getversion_nightly()
+            except Exception, e:
+                import wikipedia
+                d = wikipedia.__version__.split(' ')
+                tag = ''
+                if d == '$Id$':
+                    # wikipedia.py got imported without using svn
+                    date = 'date unknown'
+                    rev = '-1 (rev unknown)' 
+                else:
+                    date = time.strptime('T'.join(d[3:5]), '%Y-%m-%dT%H:%M:%SZ')
+                    rev = d[2] + ' (wikipedia.py)'
     datestring = time.strftime('%b %d %Y, %H:%M:%S', date)
     cache = {'tag': tag, 'rev': rev, 'date': datestring}
     return cache
@@ -67,6 +70,14 @@ def getversion_nightly():
     if not date or not tag or not rev:
         raise ParseError
     return (tag, rev, date)
+    
+def syscall(command):
+    return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0].strip()
+    
+def getversion_git():
+    date = time.gmtime(int(syscall('git show --pretty=format:%ct HEAD')))
+    rev  = syscall('git show --pretty=format:%h HEAD')
+    return (None, rev, date)
     
 if __name__ == '__main__':
     print 'Pywikipedia %s' % getversion()
