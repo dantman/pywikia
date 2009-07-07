8619eb9418061ce011df9d2e2383e1ca4c99bbef
alexsh <alexsh@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-07-07 20:14:05 +0000

    UpInfo: Manually update wesolve family
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@7029 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- families/wesolve_family.py
+++ families/wesolve_family.py
@@ -12,66 +12,27 @@ class Family(family.Family):
     def __init__(self):
         family.Family.__init__(self)
         self.name = 'wesolve'
-        self.langs = {}
-        for lang in ['en','it']:
-            self._addlang(lang,
-                        location = 'wesolveitnet.com',
-                        namespaces = {-2: u'Media',
-                                      -1: u'Special',
-                                      0: None,
-                                      1: u'Talk',
-                                      2: u'User',
-                                      3: u'User talk',
-                                      4: u'MozillaWiki',
-                                      5: u'MozillaWiki talk',
-                                      6: u'Image',
-                                      7: u'Image talk',
-                                      8: u'MediaWiki',
-                                      9: u'MediaWiki talk',
-                                      10: u'Template',
-                                      11: u'Template talk',
-                                      12: u'Help',
-                                      13: u'Help talk',
-                                      14: u'Category',
-                                      15: u'Category talk'})
+        self.langs = {
+            'wesolve': None,
+        }
+
+        self.namespaces[4] = {
+            '_default': u'Wordsandmore',
+        }
+        self.namespaces[5] = {
+            '_default': u'Wordsandmore talk',
+        }
+
 
     def version(self, code):
-        return "1.4"
+        return "1.5.7"
 
-    def put_address(self, code, name):
-        return '/wsiwiki/index.php?title=%s&action=submit'%name
+    def scriptpath(self, code):
+        return ''
 
-    def get_address(self, code, name):
-        return '/wsiwiki/index.php?title='+name+"&redirect=no"
+    def hostname(self, code):
+        return 'wordsandmore.org'
 
-    def references_address(self, code, name):
-        return ("/wsiwiki/index.php?title=%s:Whatlinkshere&target=%s&limit=%d"
-                % (self.special_namespace_url(code), name,
-                   config.special_page_limit))
-
-    def upload_address(self, code):
-        raise UploadDisabled
-
-    def allmessages_address(self, code):
-        return ("/wsiwiki/index.php?title=%s:Allmessages&ot=html" %
-                self.special_namespace_url(code))
-
-    def login_address(self, code):
-        return ('/wsiwiki/index.php?title=%s:Userlogin&amp;action=submit' %
-                self.special_namespace_url(code))
-
-    def move_address(self, code):
-        return ('/wsiwiki/index.php?title=%s:Movepage&action=submit' %
-                self.special_namespace_url(code))
-
-    def delete_address(self, code, name):
-        return '/wsiwiki/index.php?title=%s&action=delete' % name
-
-    def version_history_address(self, code, name):
-        return ('/wsiwiki/index.php?title=%s&action=history&limit=%d'
-                % (name, config.special_page_limit))
-
-    def export_address(self, code):
-        return ('/wsiwiki/index.php?title=%s:Export'
-                % self.special_namespace_url(code))
-
+    def apipath(self, code):
+        raise NotImplementedError(
+            "The wesolve family does not support api.php")
