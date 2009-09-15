7700f2c1c4c9b83fa0b9a3a6818d4419581e0ac8
filnik <filnik@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-09-15 08:37:30 +0000

    Bugfix by xqt, patch: #2858443
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@7247 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 """
-Script to perform some tests.
+Script to test whether you are logged-in
+
+Parameters:
+
+   -all         Try to test on all sites where a username is defined in
+                user-config.py.
+   -sysop       test your sysop account. (Works only with -all)
 """
 #
 # (C) Rob W.W. Hooft, 2003
@@ -9,19 +15,35 @@ Script to perform some tests.
 #
 __version__='$Id$'
 #
-import re,sys,wikipedia
+import re,sys,wikipedia,config
+
+def show (mysite):
+    if mysite.loggedInAs():
+        wikipedia.output(u"You are logged in on %s as %s." % (repr(mysite), mysite.loggedInAs()))
+    else:
+        wikipedia.output(u"You are not logged in on %s." % repr(mysite))
 
 def main():
+    testall = False
+    sysop   = False
     for arg in wikipedia.handleArgs():
-    wikipedia.output(u"Unknown argument: %s" % arg)
-    wikipedia.stopme()
-    sys.exit(1)
-
-    mysite = wikipedia.getSite()
-    if mysite.loggedInAs():
-    wikipedia.output(u"You are logged in on %s as %s." % (repr(mysite), mysite.loggedInAs()))
+        if arg == "-all":
+            testall = True
+        elif arg == "-sysop":
+            sysop = True
+        else:
+            wikipedia.showHelp()
+            return
+    if testall:
+        if sysop:
+            namedict = config.sysopnames
+        else:
+            namedict = config.usernames
+        for familyName in namedict.iterkeys():
+            for lang in namedict[familyName].iterkeys():
+                 show(wikipedia.getSite(lang, familyName))
     else:
-    wikipedia.output(u"You are not logged in on %s." % repr(mysite))
+        show(wikipedia.getSite())
 
 if __name__ == "__main__":
     try:
