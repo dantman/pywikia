9c5a0c5866d3c0c4307fb3fb04ba88a2f341789d
filnik <filnik@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-10-14 16:29:25 +0000

    Bugfix: getTemplates(), when there aren't templates it crashed, now no more
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@7462 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- wikipedia.py
+++ wikipedia.py
@@ -1054,14 +1054,15 @@ not supported by PyWikipediaBot!"""
         while True:
             data = query.GetData(params, self.site(), encodeTitle = False)
             pageid = data[u'query'][u'pages'].keys()[0]
-            for tmp in data['query']['pages'][pageid].values()[0]:
-                count += 1
-                try:
-                    tmpsFound.append(Page(self.site(), tmp['title'], defaultNamespace=tmp['ns']) )
-                except TypeError:
-                    pass
-                if count >= tllimit:
-                    break
+            if type(data['query']['pages'][pageid].values()[0]) == list(): # if it's not a list: no templates
+                for tmp in data['query']['pages'][pageid].values()[0]:
+                    count += 1
+                    try:
+                        tmpsFound.append(Page(self.site(), tmp['title'], defaultNamespace=tmp['ns']) )
+                    except TypeError: # no templates in the file.
+                        pass
+                    if count >= tllimit:
+                        break
             
             if 'query-continue' in data and count < tllimit:
                 params["tlcontinue"] = data["query-continue"]["templates"]["tlcontinue"]
