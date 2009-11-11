09d311585c6b8456bda9210b73541c7f6e55822a
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-11-11 09:17:01 +0000

    Bugfix for unicode decoder with wikipedia.output()
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@7626 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- query.py
+++ query.py
@@ -75,7 +75,7 @@ def GetData(params, site = None, useAPI = True, retryCount = 5, encodeTitle = Tr
         
         for k, v in params.iteritems():
             if k not in ['action', 'format', 'file', 'xml']:
-                wikipedia.output("%s: %s" % (k, v) )
+                wikipedia.output(u"%s: %s" % (k, v) )
         wikipedia.output(u'-' * 16 )
         
 
