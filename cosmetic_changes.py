db5e46d41b9b6856e9a492bab6264d3c37b42116
xqt <xqt@6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6>
2009-11-23 08:35:56 +0000

    ignore putSpacesInLists within <source />
    git-svn-id: http://svn.wikimedia.org/svnroot/pywikipedia/trunk/pywikipedia@7681 6a7f98fc-eeb0-4dc1-a6e2-c2c589a08aa6

--- cosmetic_changes.py
+++ cosmetic_changes.py
@@ -444,7 +444,7 @@ class CosmeticChangesToolkit:
         If there are any complaints, please file a bug report.
         """
         if not self.redirect:
-            text = pywikibot.replaceExcept(text, r'(?m)^(?P<bullet>[:;]*(\*+|#+)[:;\*#]*)(?P<char>[^\s\*#:;].+?)', '\g<bullet> \g<char>', ['comment', 'math', 'nowiki', 'pre'])
+            text = pywikibot.replaceExcept(text, r'(?m)^(?P<bullet>[:;]*(\*+|#+)[:;\*#]*)(?P<char>[^\s\*#:;].+?)', '\g<bullet> \g<char>', ['comment', 'math', 'nowiki', 'pre', 'source'])
         return text
 
     #from fixes.py
